a65a87dc217ce7f9fddf969008a546154b06dd46
Bert Bogaerts <Bert.Bogaerts@wiv-isp.be>
2017-01-12 11:39:36 +0100

    Changed resources folder into a module

---
+++ resources/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+CSS_STYLE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'css/style.css')
+
+YAML_RES_CHAR_FAST = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'yaml/resistance_characterization_fast.yml')
+YAML_TYPING_FAST = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'yaml/sequence_typing_fast.yml')
