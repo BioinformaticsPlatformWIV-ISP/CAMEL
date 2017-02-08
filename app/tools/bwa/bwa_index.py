581992ccdbb0abe78340150d332fde264c3cfcd2
Qiang FU <qiang.fu@wiv-isp.be>
2017-02-08 11:30:09 +0100

    use symlink to generate index locally to avoid possible issues

--- app/tools/bwa/bwa_index.py
+++ app/tools/bwa/bwa_index.py
@@ -40,15 +40,19 @@ class BWAIndex(BWA):
         Check FASTA_REF input and concatenate them if multiple fasta input files
         :return: None
         """
+        super(BWAIndex, self)._check_input()
+
         nb_of_inputs = len(self._tool_inputs['FASTA_REF'])
-        if nb_of_inputs > 1:
+        if nb_of_inputs == 0:
+            raise ValueError("Required reference genome (FASTA) input file is missing.")
+        elif nb_of_inputs > 1:
             multifasta_file = self.__get_multi_fasta_genome_filename()
             FileUtils.concatenate_files(multifasta_file, [f.path for f in self._tool_inputs['FASTA_REF']])
             self._refgenome_fasta = multifasta_file
-        elif nb_of_inputs == 0:
-            raise ValueError("Required reference genome (FASTA) input file is missing.")
         else:
-            self._refgenome_fasta = self._tool_inputs['FASTA_REF'][0].path
+            fasta_file_path = self._tool_inputs['FASTA_REF'][0].path
+            self._refgenome_fasta = os.path.join(self._folder, os.path.basename(fasta_file_path))
+            os.symlink(fasta_file_path, self._refgenome_fasta)
 
     def __set_output(self):
         """
