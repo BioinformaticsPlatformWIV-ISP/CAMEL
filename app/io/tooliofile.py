32d2d5a9dffb604b2cf92c13e3acc24336ac3b95
Bert Bogaerts <Bert.Bogaerts@wiv-isp.be>
2017-02-15 14:46:56 +0100

    Undo of last commit, symlink problem was caused by galaxy. Code was correct

--- app/io/tooliofile.py
+++ app/io/tooliofile.py
@@ -72,10 +72,7 @@ class ToolIOFile(ToolIO):
         Checks whether this file exists.
         :return: True if the file exists, False otherwise
         """
-        if os.path.islink(self.path):
-            return os.path.isfile(os.path.realpath(self.path))
-        else:
-            return os.path.isfile(self._path)
+        return os.path.isfile(self._path)
 
     @property
     def size(self):
