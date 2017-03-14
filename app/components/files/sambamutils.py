338d8d8c1671ec6780ff7203a2b08eb05e9afdc5
Qiang FU <qiang.fu@wiv-isp.be>
2017-03-14 23:05:38 +0100

    added sambamutils class to check empty bam file

---
+++ app/components/files/sambamutils.py
@@ -0,0 +1,27 @@
+import os
+import pysam
+
+
+class SAMBAMutils(object):
+
+    """
+    Helper to perform SAM BAM file related functions
+    """
+
+    @staticmethod
+    def is_empty(infile):
+        """
+        Check whether a SAM/BAM file contains only header
+        :return: True if infile contains no read records
+        """
+        _, file_ext = os.path.splitext(infile)
+        if file_ext.lower() == '.sam':
+            map_file = pysam.AlignmentFile(infile, "r")
+        elif file_ext.lower() == '.bam':
+            map_file = pysam.AlignmentFile(infile, "rb")
+
+        read_count = map_file.count(until_eof=True)
+        if read_count == 0:
+            return True
+        else:
+            return False
