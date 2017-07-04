a4f483ed8b5f09259bf59edc582f70640f1fa126
Raf Winand <raf.winand@wiv-isp.be>
2017-07-04 15:03:20 +0200

    Split up pipeline and step to allow easy snakemake logging

---
+++ app/pipeline/snakestep.py
@@ -0,0 +1,130 @@
+import logging
+
+from app.pipeline.step import Step
+from app.services.pipelineservice import PipelineService
+from app.services.stepservice import StepService
+
+
+class SnakeStep(Step):
+    """
+    This class represents a step in a Snakemake pipeline. It executes a single tool.
+    """
+
+    def __init__(self, name, tool, pipeline_name, job_id, camel, folder, db_logging=False):
+        """
+        Initializes the step
+        :param name: Name of the step
+        :param tool: Tool that needs to be executed
+        :param pipeline: Pipeline object of which this step is a part
+        :param camel: Camel object
+        :param folder: Working directory where the tool has to run
+        """
+        super(SnakeStep, self).__init__(name, tool, None, camel)
+        self._db_logging = db_logging
+        self._pipeline_service = PipelineService(pipeline_name, camel.connection)
+        self.__set_step_id()
+        self._folder = folder
+        self._job_id = job_id
+
+    def __set_step_id(self):
+        """
+        Sets the step id when logging is requested in the pipeline (= a pipeline service is created)
+        :return: None
+        """
+        if self._db_logging is True:
+            self.step_id = self._pipeline_service.get_step_id(self.name)
+            if self.step_id is None:
+                self.step_id = self._pipeline_service.insert_step(self.name)
+
+    def run_step(self):
+        """
+        Runs the current step.
+        :return: None
+        """
+        self._tool.add_input_files(self._step_inputs)
+        self._tool.add_input_informs(self._input_informs)
+        logging.info("Default parameters loaded: {}".format(self._tool.parameter_overview))
+        self.__set_pipeline_options()
+        self._add_pipeline_parameters()
+        self._add_job_parameters()
+        self._tool.run(self._folder)
+        logging.info('Step output: {}'.format(list(self.outputs.items())))
+        logging.info('Step informs: {}'.format(list(self.informs.items())))
+        if self._db_logging is True:
+            self._log_outputs()
+            self._log_job_parameters()
+
+    @property
+    def name(self):
+        """
+        Returns the name of this step.
+        :return: Name
+        """
+        return self._name
+
+    @property
+    def step_id(self):
+        """
+        Returns the database id of this step.
+        :return: Database id
+        """
+        return self._step_id
+
+    @step_id.setter
+    def step_id(self, id_):
+        """
+        Sets the database id.
+        :return: None
+        """
+        self._step_id = id_
+        self._step_service = StepService(id_, self._camel.connection)
+
+    def add_inputs(self, dict_):
+        """
+        Adds the inputs to the step
+        :param dict_: Dictionary with input objects
+        :return: None
+        """
+        self._step_inputs = dict_
+
+    def add_informs(self, dict_):
+        """
+        Adds informs to the step.
+        :param dict_: Dictionary with the informs
+        :return: None
+        """
+        self._input_informs = dict_
+        logging.info("Inform added: {}".format(dict_))
+
+    def __set_pipeline_options(self):
+        """
+        Loads options from the database.
+        :return: None
+        """
+        logging.info("Loading pipeline parameters from database")
+        if self._pipeline_service is not None:
+            for parameter in self._pipeline_service.get_pipeline_step_parameters(self.name):
+                self.add_pipeline_options({parameter.name: parameter.value})
+
+    def _log_outputs(self):
+        """
+        Logs the outputs in the database.
+        :return: None
+        """
+        for key, files in self.outputs.items():
+            for i in range(0, len(files)):
+                if files[i].logged:
+                    output_data = [self._job_id, self.step_id, files[i].TYPE_NAME, key, i, files[i].hash]
+                    logging.debug('OUTPUT DATA: {}'.format(output_data))
+                    self._step_service.log_output(output_data)
+                    logging.debug('Output {} ({}) logged'.format(key, i))
+
+    def _log_job_parameters(self):
+        """
+        Logs the job parameters in the database.
+        :return: None
+        """
+        for parameter_name, parameter_value in self._job_options.items():
+            parameter_id = self._step_service.get_parameter_id(self._tool.tool_id, parameter_name)
+            self._step_service.log_job_parameter(parameter_id, self.step_id, self._job_id, parameter_value)
+            logging.debug("Job parameter '{}' logged".format(parameter_name))
--- app/pipeline/step.py
+++ app/pipeline/step.py
@@ -1,33 +1,23 @@
-import logging
-import os
-from collections import namedtuple
-
-from app.components.filesystemhelper import FileSystemHelper
-from app.loggers.logmanager import LogManager
 from app.services.stepservice import StepService
+import logging
 
 
 class Step(object):
     """
     This class represents a step in a pipeline. It executes a single tool.
     """
-    StepInput = namedtuple('StepInput', ('name', 'source', 'alias', 'required',))
-    ExternalInput = namedtuple('ExternalInput', ('files', 'alias',))
-    StepInform = namedtuple('StepInform', ('source', 'alias'))
-
-    ConditionalNextStep = namedtuple('ConditionalNextStep', ('condition', 'step_id'))
-    DefaultNextStep = namedtuple('DefaultNextStep', ('step_id',))
 
     def __init__(self, name, tool, pipeline, camel):
         """
         Initializes a step.
+        :param name: Name of the step
+        :param tool: Tool that needs to be executed
+        :param pipeline: Pipeline object of which this step is a part
+        :param camel: Camel object
         """
         self._name = name
         self._tool = tool
         self._step_id = None
-        self._input_specification = None
-        self._inform_specification = None
-        self._next_step_specification = None
         self._folder = None
         self._step_inputs = {}
         self._input_informs = {}
@@ -36,26 +26,6 @@ class Step(object):
         self._pipeline_options = {}
         self._job_options = {}
         self._step_service = None
-
-    def run_step(self):
-        """
-        Runs the current step.
-        :return: None
-        """
-        self._create_folder()
-        LogManager.attach_step_handlers(self._folder)
-        self._tool.add_input_files(self._step_inputs)
-        self._tool.add_input_informs(self._input_informs)
-        logging.info("Default parameters loaded: {}".format(self._tool.parameter_overview))
-        self._add_pipeline_parameters()
-        self._add_job_parameters()
-        self._tool.run(self._folder)
-        logging.info('Step output: {}'.format(list(self.outputs.items())))
-        logging.info('Step informs: {}'.format(list(self.informs.items())))
-        if self._pipeline.job_id is not None:
-            self._log_outputs()
-            self._log_job_parameters()
-        LogManager.detach_step_handlers()
 
     @property
     def name(self):
@@ -81,117 +51,6 @@ class Step(object):
         """
         self._step_id = id_
         self._step_service = StepService(id_, self._camel.connection)
-
-    @property
-    def input_specification(self):
-        """
-        Returns the input specification.
-        :return: Input specification
-        """
-        specification = []
-        if self._input_specification is None:
-            return specification
-        for item in self._input_specification:
-            try:
-                if 'external-input' in item:
-                    files = [item['type'](element) for element in item['external-input']]
-                    specification.append(Step.ExternalInput(files, item['alias']))
-                else:
-                    alias = item['alias'] if 'alias' in item else item['name']
-                    required = item.get('required', False)
-                    specification.append(Step.StepInput(item['name'], item['from'], alias, required))
-            except KeyError as err:
-                raise ValueError("'{}' missing from input specification: {}".format(err, list(item.items())))
-        return specification
-
-    @input_specification.setter
-    def input_specification(self, specification):
-        """
-        Sets the input specification for this step.
-        :param specification: Input specification
-        :return: None
-        """
-        self._input_specification = specification
-
-    @property
-    def inform_specification(self):
-        """
-        Returns the inform specification.
-        :return: Inform specification
-        """
-        specification = []
-        if self._inform_specification is None:
-            return specification
-        for item in self._inform_specification:
-            try:
-                specification.append(Step.StepInform(item['from'], item['alias']))
-            except KeyError as err:
-                raise ValueError("'{}' missing from inform specification: {}".format(err, list(item.items())))
-        return specification
-
-    @inform_specification.setter
-    def inform_specification(self, specification):
-        """
-        Sets the inform specification for this step.
-        :param specification: Inform specification
-        :return: None
-        """
-        self._inform_specification = specification
-
-    @property
-    def next_step_specification(self):
-        """
-        Returns the specification of the next step.
-        :return: Specification
-        """
-        if not self._next_step_specification:
-            return None
-        specification = []
-        for branch in self._next_step_specification:
-            try:
-                if 'when' in branch:
-                    specification.append(Step.ConditionalNextStep(branch['when']['condition'],
-                                                                  branch['when']['step-id']))
-                elif 'default' in branch:
-                    specification.append(Step.DefaultNextStep(branch['default']['step-id']))
-                else:
-                    raise ValueError("Next step specification is not valid: {}".format(branch))
-            except KeyError as err:
-                raise ValueError("{} missing from step specification".format(err))
-        return specification
-
-    @next_step_specification.setter
-    def next_step_specification(self, specification):
-        """
-        Sets the specification for the next step.
-        :param specification: Specification
-        :return: None
-        """
-        self._next_step_specification = specification
-
-    def add_input(self, key, new_input):
-        """
-        Adds input to the step.
-        :param key: Key for the input
-        :param new_input: List of the new inputs
-        :return: None
-        """
-        if len(new_input) == 0:
-            return
-        if key not in self._step_inputs:
-            self._step_inputs[key] = []
-        self._step_inputs[key].extend(new_input)
-        logging.info("Input '{}' added: {}".format(key, new_input))
-
-    def add_inform(self, key, inform):
-        """
-        Adds inform to the step.
-        :param key: Key for the inform
-        :param inform: Inform
-        :return: None
-        """
-        self._input_informs[key] = inform
-        logging.info("Inform '{}' added: {}".format(key, inform))
 
     @property
     def outputs(self):
@@ -225,15 +84,6 @@ class Step(object):
         """
         self._job_options.update(options)
 
-    def _create_folder(self):
-        """
-        Creates the folder for this step.
-        :return: None
-        """
-        self._folder = os.path.join(self._pipeline.folder, FileSystemHelper.make_valid(self.name))
-        if not os.path.isdir(self._folder):
-            os.makedirs(self._folder)
-
     def _add_pipeline_parameters(self):
         """
         Adds the pipeline parameters.
@@ -251,25 +101,3 @@ class Step(object):
         if len(self._job_options) > 0:
             logging.info("Adding job parameters")
             self._tool.update_parameters(**self._job_options)
-
-    def _log_outputs(self):
-        """
-        Logs the outputs in the database.
-        :return: None
-        """
-        for key, files in self.outputs.items():
-            for i in range(0, len(files)):
-                if files[i].logged:
-                    output_data = [self._pipeline.job_id, self.step_id, files[i].TYPE_NAME, key, i, files[i].hash]
-                    self._step_service.log_output(output_data)
-                    logging.debug('Output {} ({}) logged'.format(key, i))
-
-    def _log_job_parameters(self):
-        """
-        Logs the job parameters in the database.
-        :return: None
-        """
-        for parameter_name, parameter_value in self._job_options.items():
-            parameter_id = self._step_service.get_parameter_id(self._tool.tool_id, parameter_name)
-            self._step_service.log_job_parameter(parameter_id, self.step_id, self._pipeline.job_id, parameter_value)
-            logging.debug("Job parameter '{}' logged".format(parameter_name))
