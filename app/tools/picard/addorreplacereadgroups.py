cf0777fe32a2414fbe0b3a61c95f91daf73177d9
Qiang FU <qiang.fu@wiv-isp.be>
2017-08-21 17:05:37 +0200

    removed default sample_name from class, now stored in tool parameters in DB, fixed a bug when those parameters are overwriten by _tool_input in picard addorreplacereadgroups

--- app/tools/picard/addorreplacereadgroups.py
+++ app/tools/picard/addorreplacereadgroups.py
@@ -6,7 +6,6 @@ class AddOrReplaceReadGroups(Picard):
     """
     Class for Picard AddOrReplaceReadGroups function
     """
-    DEFAULT_SAMPLE_NAME = 'sampleA'
 
     def __init__(self, camel):
         """
@@ -24,7 +23,6 @@ class AddOrReplaceReadGroups(Picard):
         :return: None
         """
         if 'SAMPLE_NAME' in self._tool_inputs:
-            sample_name = self._tool_inputs['SAMPLE_NAME'][0].value
-        else:
-            sample_name = AddOrReplaceReadGroups.DEFAULT_SAMPLE_NAME
-        self._input_string += " RGSM={0} RGID={0}".format(sample_name)
+            # if SAMPLE_NAME specified, it will replace the default values of parameters: RG_sample_name, RG_id in DB
+            self._specific_parameters = ['RG_id', 'RG_sample_name']
+            self._input_string += " RGSM={0} RGID={0}".format(self._tool_inputs['SAMPLE_NAME'][0].value)
