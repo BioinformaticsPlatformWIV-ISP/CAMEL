731e5b0ad697e7bf9641ce66175a1535234b43bc
Raf Winand <raf.winand@wiv-isp.be>
2017-11-13 14:35:14 +0100

    File formatting (whitespace and blank lines)

--- scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.py
+++ scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.py
@@ -12,7 +12,6 @@ import time
 import datetime
 
 
-
 class GATKSomaticMain(object):
     """
     Main class to run the GATK somatic variant caller pipeline.
@@ -21,7 +20,6 @@ class GATKSomaticMain(object):
     DB_LOGGING = True
     DEBUG = True
     SNAKEFILE = os.path.join(os.path.dirname(__file__), 'gatk_somatic_steps.snakefile')
-
 
     def __init__(self):
         """
@@ -33,8 +31,7 @@ class GATKSomaticMain(object):
         self.pipeline = None
 
         # Name of config file generated at runtime for snakemake pipeline
-        self.runtime_config_name = os.path.join(os.getcwd(),'runtime_config.yaml')
-
+        self.runtime_config_name = os.path.join(os.getcwd(), 'runtime_config.yaml')
 
     def __parse_command_line(self):
         """
@@ -48,26 +45,32 @@ class GATKSomaticMain(object):
 
         # input
         gp = ap.add_mutually_exclusive_group(required=True)
-        gp.add_argument('-PE', '--Paired_end', metavar='fq_file', dest='paired_end', help='Paired-end fastq files.', nargs='+')
-        gp.add_argument('-SE', '--Single_end', metavar='fq_file', dest='single_end', help='Single-end fastq files.', nargs='+')
+        gp.add_argument('-PE', '--Paired_end', metavar='fq_file', dest='paired_end', help='Paired-end fastq files.',
+                        nargs='+')
+        gp.add_argument('-SE', '--Single_end', metavar='fq_file', dest='single_end', help='Single-end fastq files.',
+                        nargs='+')
 
         # output
         ap.add_argument('--vcf_output', dest='vcf_output', metavar='vcf_output', help='Output vcf file')
         ap.add_argument('--tab_output', dest='tab_output', metavar='tab_output', help='Output variant-call table file')
-        ap.add_argument('--covar_output', dest='covar_output', metavar='covar_output', help='Output covariates analysis pdf')
+        ap.add_argument('--covar_output', dest='covar_output', metavar='covar_output',
+                        help='Output covariates analysis pdf')
         ap.add_argument('--bam_output', dest='bam_output', metavar='bam_output', help='Aligned BAM file')
 
         # references
-        ap.add_argument('-R','--fasta_ref', dest='fasta_ref', metavar='fasta_ref', help='Human genome reference fasta file name (as in db_loc).', required=True)
-        ap.add_argument('-S','--vcf_snps', metavar='vcf_snps', dest='vcf_known_snps', help='Known variant sites (snps) vcf file name (as in db_loc).', required=True)
-        ap.add_argument('-I','--vcf_indels', metavar='vcf_indels', dest='vcf_known_indels', help='Known variant sites (indels) vcf file name (as in db_loc).', required=True)
+        ap.add_argument('-R', '--fasta_ref', dest='fasta_ref', metavar='fasta_ref',
+                        help='Human genome reference fasta file name (as in db_loc).', required=True)
+        ap.add_argument('-S', '--vcf_snps', metavar='vcf_snps', dest='vcf_known_snps',
+                        help='Known variant sites (snps) vcf file name (as in db_loc).', required=True)
+        ap.add_argument('-I', '--vcf_indels', metavar='vcf_indels', dest='vcf_known_indels',
+                        help='Known variant sites (indels) vcf file name (as in db_loc).', required=True)
 
         # MarkDuplicates flag
         ap.add_argument('--mark_duplicates', dest='markduplicates', help='Mark duplicate reads.', action='store_true')
-        ap.add_argument('--job_id', dest='job_id', metavar='job_id', help='Job ID for debugging and logging.', default=datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d_%H%M%S-%f"))
+        ap.add_argument('--job_id', dest='job_id', metavar='job_id', help='Job ID for debugging and logging.',
+                        default=datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d_%H%M%S-%f"))
 
         return ap.parse_args()
-
 
     def __generate_config_file(self):
         """
@@ -89,11 +92,11 @@ class GATKSomaticMain(object):
         # References
         self.config_data['fasta_ref'] = self._args.fasta_ref
         self.config_data['vcf_known_snps'] = self._args.vcf_known_snps
-        self.config_data['vcf_known_indels']=self._args.vcf_known_indels
+        self.config_data['vcf_known_indels'] = self._args.vcf_known_indels
 
         # Input fastq files
         if self._args.paired_end:
-            self.config_data['fastq'] =self._args.paired_end
+            self.config_data['fastq'] = self._args.paired_end
             self.config_data['PE'] = True
             self.config_data['SE'] = False
         if self._args.single_end:
@@ -117,7 +120,6 @@ class GATKSomaticMain(object):
         # Create and write to config file
         with open(self.runtime_config_name, 'w') as handle:
             yaml.dump(self.config_data, handle)
-
 
     def run(self):
         """
@@ -149,11 +151,13 @@ class GATKSomaticMain(object):
         command = Command(to_execute)
         command.run_command(os.getcwd(), subprocess.STDOUT)
         if command.returncode != 0:
-            with open("/scratch/temp/galaxy_logs/{}_Stdout".format(self._args.job_id),"w") as file_out:
+            with open("/scratch/temp/galaxy_logs/{}_Stdout".format(self._args.job_id), "w") as file_out:
                 file_out.write(command.stdout)
-            with open("/scratch/temp/galaxy_logs/{}_Stderr".format(self._args.job_id),"w") as file_out:
+            with open("/scratch/temp/galaxy_logs/{}_Stderr".format(self._args.job_id), "w") as file_out:
                 file_out.write(command.stderr)
-            raise RuntimeError("Error executing Snakemake. Check log ('/scratch/temp/galaxy_logs/{}_Stderr') for more information.".format(self._args.job_id))
+            raise RuntimeError(
+                "Error executing Snakemake. Check log ('/scratch/temp/galaxy_logs/{}_Stderr') for more information.".format(
+                    self._args.job_id))
 
 
 if __name__ == '__main__':
