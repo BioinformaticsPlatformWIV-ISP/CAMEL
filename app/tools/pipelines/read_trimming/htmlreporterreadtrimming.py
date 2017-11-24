59a072d913d642cb5290754f5dac24d205b56434
Bert Bogaerts <Bert.Bogaerts@wiv-isp.be>
2017-11-24 11:43:17 +0100

    Added the novel HTML classes to the read trimming pipeline

--- app/tools/pipelines/read_trimming/htmlreporterreadtrimming.py
+++ app/tools/pipelines/read_trimming/htmlreporterreadtrimming.py
@@ -1,10 +1,12 @@
 import os
 
+from app.components.html.htmlreportsection import HtmlReportSection
 from app.error.invalidinputspecificationerror import InvalidInputSpecificationError
-from app.tools.export.htmlreporter import HtmlReporter
+from app.io.tooliovalue import ToolIOValue
+from app.tools.tool import Tool
 
 
-class HtmlReporterReadTrimming(HtmlReporter):
+class HtmlReporterReadTrimming(Tool):
     """
     Tool to create HTML reports for the read trimming.
     """
@@ -15,19 +17,20 @@ class HtmlReporterReadTrimming(HtmlReporter):
         :param camel: CAMEL instance
         :return: None
         """
-        super(HtmlReporterReadTrimming, self).__init__(camel)
-        self.__subfolder = 'quality_control'
+        super(HtmlReporterReadTrimming, self).__init__('HTML Reporter', '0.1', camel)
+        self.__subfolder = 'read_trimming'
+        self._report_section = None
 
-    def _create_report(self):
+    def _execute_tool(self):
         """
-        Creates the HTML report.
+        Executes this tool.
         :return: None
         """
-        self._report.add_header('Quality Control', 2)
+        self._report_section = HtmlReportSection('Read Trimming')
         self.__add_section_qc_pre()
         self.__add_trimming_section()
         self.__add_qc_post_section()
-        self._report.add_horizontal_line()
+        self._tool_outputs['VAL_HTML'] = [ToolIOValue(self._report_section, False)]
 
     def _check_input(self):
         """
@@ -49,29 +52,29 @@ class HtmlReporterReadTrimming(HtmlReporter):
         Adds the pre trimming QC section
         :return: None
         """
-        self._report.add_header('Pre-trimming', 3)
+        self._report_section.add_header('Pre-trimming', 3)
         for fastqc_report, orientation in zip(self._tool_inputs['HTML_Pre'], ('forward', 'reverse')):
             relative_path = os.path.join(self.__subfolder, 'pre_trimming', 'fastqc_report_{}.html'.format(orientation))
-            self._save_file(fastqc_report.path, relative_path)
-            self._report.add_link_to_file('FastQC report ({})'.format(orientation), relative_path)
+            self._report_section.add_file(fastqc_report.path, relative_path)
+            self._report_section.add_link_to_file('FastQC report ({})'.format(orientation), relative_path)
 
     def __add_qc_post_section(self):
         """
         Adds the post trimming QC section
         :return: None
         """
-        self._report.add_header('Post-trimming', 3)
+        self._report_section.add_header('Post-trimming', 3)
         for fastqc_report, orientation in zip(self._tool_inputs['HTML_Post'], ('forward', 'reverse')):
             relative_path = os.path.join(self.__subfolder, 'post_trimming', 'fastqc_report_{}.html'.format(orientation))
-            self._save_file(fastqc_report.path, relative_path)
-            self._report.add_link_to_file('FastQC report ({})'.format(orientation), relative_path)
+            self._report_section.add_file(fastqc_report.path, relative_path)
+            self._report_section.add_link_to_file('FastQC report ({})'.format(orientation), relative_path)
 
     def __add_trimming_section(self):
         """
         Adds the read trimming section.
         :return: None
         """
-        self._report.add_header('Read Trimming', 3)
+        self._report_section.add_header('Read Trimming', 3)
         trimming_info = self._input_informs['trimming']
         table_data = [
             ['Input Reads Pairs:', trimming_info['paired_reads_in']],
@@ -80,7 +83,7 @@ class HtmlReporterReadTrimming(HtmlReporter):
             ['Reverse Only Surviving:', trimming_info['reverse_only_reads']],
             ['Dropped:', trimming_info['reads_drop']]
         ]
-        self._report.add_table(table_data, table_attributes=[('class', 'information')])
+        self._report_section.add_table(table_data, table_attributes=[('class', 'information')])
         self.__add_trimmed_reads()
 
     def __add_trimmed_reads(self):
@@ -91,20 +94,20 @@ class HtmlReporterReadTrimming(HtmlReporter):
         for trimmed_reads_paired, orientation in zip(self._tool_inputs['FASTQ_PE'], ('forward', 'reverse')):
             filename = 'trimmed_reads_paired_{}.fastq'.format(orientation)
             relative_path = os.path.join(self.__subfolder, 'read_trimming', filename)
-            self._save_file(trimmed_reads_paired.path, relative_path)
-            self._report.add_link_to_file('Trimmed reads paired ({})'.format(orientation), relative_path)
-        self._report.add_line_break()
+            self._report_section.add_file(trimmed_reads_paired.path, relative_path)
+            self._report_section.add_link_to_file('Trimmed reads paired ({})'.format(orientation), relative_path)
+        self._report_section.add_line_break()
 
         if 'FASTQ_SE_FORWARD' in self._tool_inputs:
             relative_path = os.path.join(self.__subfolder, 'read_trimming', 'trimmed_reads_unpaired_forward.fastq')
-            self._save_file(self._tool_inputs['FASTQ_SE_FORWARD'][0].path, relative_path)
-            self._report.add_link_to_file('Trimmed reads unpaired (forward)', relative_path)
+            self._report_section.add_file(self._tool_inputs['FASTQ_SE_FORWARD'][0].path, relative_path)
+            self._report_section.add_link_to_file('Trimmed reads unpaired (forward)', relative_path)
         else:
-            self._report.add_link_to_file('Trimmed reads unpaired (forward)', None)
+            self._report_section.add_link_to_file('Trimmed reads unpaired (forward)', None)
 
         if 'FASTQ_SE_REVERSE' in self._tool_inputs:
             relative_path = os.path.join(self.__subfolder, 'read_trimming', 'trimmed_reads_unpaired_reverse.fastq')
-            self._save_file(self._tool_inputs['FASTQ_SE_REVERSE'][0].path, relative_path)
-            self._report.add_link_to_file('Trimmed reads unpaired (reverse)', relative_path)
+            self._report_section.add_file(self._tool_inputs['FASTQ_SE_REVERSE'][0].path, relative_path)
+            self._report_section.add_link_to_file('Trimmed reads unpaired (reverse)', relative_path)
         else:
-            self._report.add_link_to_file('Trimmed reads unpaired (reverse)', None)
+            self._report_section.add_link_to_file('Trimmed reads unpaired (reverse)', None)
