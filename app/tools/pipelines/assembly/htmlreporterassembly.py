64d43d3176c1e8dd9873038b2709457d0aa27a3d
Bert Bogaerts <Bert.Bogaerts@wiv-isp.be>
2017-11-24 15:38:20 +0100

    Updated the assembly reporter to the novel HTML classes

--- app/tools/pipelines/assembly/htmlreporterassembly.py
+++ app/tools/pipelines/assembly/htmlreporterassembly.py
@@ -1,10 +1,13 @@
 import os
 
+from app.components.filesystemhelper import FileSystemHelper
+from app.components.html.htmlreportsection import HtmlReportSection
 from app.error.invalidinputspecificationerror import InvalidInputSpecificationError
-from app.tools.export.htmlreporter import HtmlReporter
+from app.io.tooliovalue import ToolIOValue
+from app.tools.tool import Tool
 
 
-class HtmlReporterAssembly(HtmlReporter):
+class HtmlReporterAssembly(Tool):
     """
     Tool to create HTML reports for the Assembly.
     """
@@ -15,18 +18,19 @@ class HtmlReporterAssembly(HtmlReporter):
         :param camel: CAMEL instance
         :return: None
         """
-        super(HtmlReporterAssembly, self).__init__(camel)
+        super(HtmlReporterAssembly, self).__init__('HTML Reporter', '0.1', camel)
         self.__subfolder = 'assembly'
+        self._report_section = None
 
-    def _create_report(self):
+    def _execute_tool(self):
         """
-        Creates the HTML report.
+        Executes this tool.
         :return: None
         """
-        self._report.add_header('Assembly', 2)
+        self._report_section = HtmlReportSection('Assembly')
         self.__add_assembly_info(self._tool_inputs['ASSEMBLER'][0].value)
         self.__add_assembly_download_link()
-        self._report.add_horizontal_line()
+        self._tool_outputs['VAL_HTML'] = [ToolIOValue(self._report_section, False)]
 
     def _check_input(self):
         """
@@ -41,7 +45,7 @@ class HtmlReporterAssembly(HtmlReporter):
             raise InvalidInputSpecificationError("No assembler input found")
         if 'quast' not in self._input_informs:
             raise InvalidInputSpecificationError("Quast informs are required")
-        super(HtmlReporterAssembly, self)._check_input()
+        super()._check_input()
 
     def __add_assembly_info(self, assembler):
         """
@@ -57,14 +61,14 @@ class HtmlReporterAssembly(HtmlReporter):
             ('Number of contigs (>1000bp):', quast_informs['contig'].get('# contigs (>= 1000 bp)', '-')),
             ('Total length:', quast_informs['genome'].get('Total length'))
         ]
-        self._report.add_table(table_data, table_attributes=[('class', 'information')])
+        self._report_section.add_table(table_data, table_attributes=[('class', 'information')])
 
     def __add_assembly_download_link(self):
         """
         Adds a download link for the assembly.
         :return: None
         """
-        assembly_path = os.path.join(self.__subfolder, '{}_contigs.fasta'.format(
-            self._tool_inputs['SAMPLE_NAME'][0].value))
-        self._save_file(self._tool_inputs['FASTA_Contig'][0].path, assembly_path)
-        self._report.add_link_to_file('Assembly (FASTA)', assembly_path)
+        relative_path = os.path.join(self.__subfolder, '{}_contigs.fasta'.format(
+            FileSystemHelper.make_valid(self._tool_inputs['SAMPLE_NAME'][0].value)))
+        self._report_section.add_file(self._tool_inputs['FASTA_Contig'][0].path, relative_path)
+        self._report_section.add_link_to_file('Assembly (FASTA)', relative_path)
