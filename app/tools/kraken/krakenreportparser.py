66bd38c973e4557b5f48ece6606160568a7315dd
Qiang FU <qiang.fu@wiv-isp.be>
2017-12-13 15:02:59 +0100

    added contamination check with kraken: krakenreportparser htmlreportercontamination; updated to include also Unclassified information

---
+++ app/tools/kraken/krakenreportparser.py
@@ -0,0 +1,53 @@
+from app.error.invalidinputspecificationerror import InvalidInputSpecificationError
+from app.tools.tool import Tool
+
+
+class KrakenReportParser(Tool):
+
+    """
+    Parses Kraken output reports.
+    """
+
+    def __init__(self, camel):
+        """
+        Initializes this tool.
+        :param camel: CAMEL instance
+        :return: None
+        """
+        super().__init__('Kraken Report Parser', '0.1', camel)
+
+    def _execute_tool(self):
+        """
+        Executes this tool.
+        :return: None
+        """
+        self._informs['contaminants_warn'] = []
+        self._informs['contaminants_fail'] = []
+        with open(self._tool_inputs['TSV'][0].path) as handle:
+            for line in handle.readlines():
+                parts = line.split('\t')
+                if parts[3] == 'U':
+                    self._informs['unclassified'] = ('Unclassified', float(parts[0]))
+                if parts[3] != 'S':
+                    continue
+                percentage = float(parts[0])
+                if percentage < float(self._parameters['threshold_warn'].value):
+                    continue
+                species_name = parts[-1].strip()
+                if species_name == self._parameters['expected_species'].value:
+                    self._informs['expected'] = (species_name, percentage)
+                elif percentage < float(self._parameters['threshold_fail'].value):
+                    self._informs['contaminants_warn'].append((species_name, percentage,))
+                else:
+                    self._informs['contaminants_fail'].append((species_name, percentage,))
+        self._informs['contaminants_warn'].sort(key=lambda x: -x[1])
+        self._informs['contaminants_fail'].sort(key=lambda x: -x[1])
+
+    def _check_input(self):
+        """
+        Checks the tool input.
+        :return: None
+        """
+        if 'TSV' not in self._tool_inputs:
+            raise InvalidInputSpecificationError("TSV input is required.")
+        super()._check_input()
---
+++ app/tools/pipelines/quality_checks/htmlreportercontamination.py
@@ -0,0 +1,66 @@
+import os
+
+from app.components.html.htmlreportsection import HtmlReportSection
+from app.components.html.htmltablecell import HtmlTableCell
+from app.error.invalidinputspecificationerror import InvalidInputSpecificationError
+from app.io.tooliovalue import ToolIOValue
+from app.tools.tool import Tool
+
+
+class HtmlReporterContamination(Tool):
+
+    """
+    HTML reporter for the contamination check step.
+    """
+
+    def __init__(self, camel):
+        """
+        Initializes this tool.
+        :param camel: CAMEL instance
+        """
+        super().__init__('HTML Reporter: Contamination', '0.1', camel)
+        self._subfolder = 'contamination_check'
+        self._report_section = HtmlReportSection("Contamination Check")
+
+    def _execute_tool(self):
+        """
+        Executes this tool.
+        :return: None
+        """
+        self.__add_species_table()
+        self.__add_krona_report()
+        self._tool_outputs['VAL_HTML'] = [ToolIOValue(self._report_section)]
+
+    def _check_input(self):
+        """
+        Checks if the provided input is valid.
+        :return: None
+        """
+        if 'HTML_Krona' not in self._tool_inputs:
+            raise InvalidInputSpecificationError("Krona report input is required.")
+        if 'species' not in self._input_informs:
+            raise InvalidInputSpecificationError("Species input is required")
+        super()._check_input()
+
+    def __add_species_table(self):
+        """
+        Adds a table containing the detected species and corresponding percentages.
+        :return: None
+        """
+        header = ['Species', 'Percentage']
+        table_data = [[HtmlTableCell(v, 'green') for v in self._input_informs['species']['expected']]]
+        for species_name, percentage in self._input_informs['species']['contaminants_fail']:
+            table_data.append([HtmlTableCell(v, 'red') for v in (species_name, percentage)])
+        for species_name, percentage in self._input_informs['species']['contaminants_warn']:
+            table_data.append([HtmlTableCell(v, 'orange') for v in (species_name, percentage)])
+        table_data.append(HtmlTableCell(v, 'grey') for v in self._input_informs['species']['unclassified'])
+        self._report_section.add_table(table_data, header, [('class', 'data')])
+
+    def __add_krona_report(self):
+        """
+        Adds a download link to the Krona report.
+        :return: None
+        """
+        relative_path = os.path.join(self._subfolder, 'krona_report.html')
+        self._report_section.add_file(self._tool_inputs['HTML_Krona'][0].path, relative_path)
+        self._report_section.add_link_to_file('Krona Report', relative_path)
