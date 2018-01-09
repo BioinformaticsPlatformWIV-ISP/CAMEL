d6cf479bc5c67413d74a7a8e840b796eb78d3c0f
Qiang FU <qiang.fu@wiv-isp.be>
2018-01-09 16:23:40 +0100

    add missing subworkflows/__init__.py file

---
+++ scripts/listeriapipeline/subworkflows/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+__CURRENT_DIR = os.path.dirname(__file__)
+
+WORKFLOW_ASSEMBLY = os.path.join(__CURRENT_DIR, 'assembly.snakefile')
+WORKFLOW_CONTAMINATION_CHECK = os.path.join(__CURRENT_DIR, 'contamination_check.snakefile')
+WORKFLOW_INIT_REPORT = os.path.join(__CURRENT_DIR, 'init_report.snakefile')
+WORKFLOW_GENE_DETECTION = os.path.join(__CURRENT_DIR, 'gene_detection.snakefile')
+WORKFLOW_READ_TRIMMING = os.path.join(__CURRENT_DIR, 'read_trimming.snakefile')
+WORKFLOW_SEQUENCE_TYPING = os.path.join(__CURRENT_DIR, 'sequence_typing.snakefile')
+WORKFLOW_QUALITY_CHECKS = os.path.join(__CURRENT_DIR, 'quality_checks.snakefile')
