1534db4a6025bc53878f3c1ca696d13cf5bf5de3
Tdelcourt <thomas.delcourt@wiv-isp.be>
2018-02-15 11:42:46 +0100

    creation gatkmutect2

---
+++ app/tools/gatk/gatkmutect2.py
@@ -0,0 +1,91 @@
+import os
+
+from app.tools.gatk.gatk import GATK
+from app.io.tooliofile import ToolIOFile
+
+
+class GATKMuTect2(GATK):
+    """
+    ==================
+    GATK MuTect2 v 3.7
+    ==================
+    Performs variant calling.
+    "MuTect2 is a somatic SNP and indel caller that combines the DREAM challenge-winning somatic genotyping engine of the original MuTect (Cibulskis et al., 2013) with the assembly-based machinery of HaplotypeCaller."
+    web: https://software.broadinstitute.org/gatk/documentation/tooldocs/3.8-0/org_broadinstitute_gatk_tools_walkers_cancer_m2_MuTect2.php
+
+    Required inputs:
+    ----------------
+    "BAM_TUMOR":        ToolIOFile object. BAM file with tumour data
+
+    Optional input:
+    ---------------
+    "BAM_NORMAL":       ToolIOFile object. BAM file with normal data for tumor-normal matching.
+    "FASTA_REF":        ToolIOFile object. FASTA file containing the reference genome.
+    "VCF_DBSNP":        ToolIOFile object. DbSNP reference vcf file location.
+    "TXT_intervals":    ToolIOFile object. Intervals list to restrict search by GATK. Accelerates analysis. Bed or GATK intervals list 
+
+    Output:
+    -------
+    "TXT_CALL_STATS":   ToolIOFile object. GATK Call stats text based file. Parse-able by scripts or in excel sheets.
+    "VCF":              ToolIOFile object. VCF file.
+
+    Mandatory parameters:
+    ---------------------
+    - output_callstats_file     default value:  call_stats.txt
+    - output_vcf_file           default value:  variant_call.vcf
+    """
+
+    def __init__(self, camel):
+        """
+        Initialize GATKMuTect2 tool.
+        :param camel: Camel instance
+        :return: None
+        """
+        super(GATKMuTect2, self).__init__('gatk MuTect2', '3.7', camel)
+
+        self._function_name = 'MuTect2'
+        self._required_inputs = ['BAM_TUMOR', 'FASTA_REF']
+
+    def _set_input(self):
+        """
+        Add the input specification in the input_string: 
+        - reference fasta (in parent class)
+        - intervals to work on (in parent class, optional)
+        - DBSNP VCF file
+        - tumour BAM file
+        - normal tissue BAM file (optional)
+        :return: None
+        """
+        super(GATKMuTect2, self)._set_input()
+
+        # required tumor bam file
+        self._input_string += "-I:tumor {} ".format(self._tool_inputs['BAM_TUMOR'][0].path)
+
+        # optional normal bam file
+        if 'BAM_NORMAL' in self._tool_inputs:
+            self._input_string += "-I:normal {} ".format(self._tool_inputs['BAM_NORMAL'][0].path)
+
+        # optional reference dbSNP vcf
+        if 'VCF_DBSNP' in self._tool_inputs:
+            self._input_string += "--dbsnp {} ".format(self._tool_inputs['VCF_DBSNP'][0].path)
+
+        # optional reference cosmic vcf
+        if 'VCF_COSMIC' in self._tool_inputs:
+            self._input_string += "--cosmic {} ".format(self._tool_inputs['VCF_COSMIC'][0].path)
+
+        # optional panel of normals vcf
+        if 'VCF_PON' in self._tool_inputs:
+            self._input_string += "--normal_panel {} ".format(self._tool_inputs['VCF_PON'][0].path)
+
+    def _set_output(self):
+        """
+        Supersedes parent class function.
+        Set the output specifications in the Camel ouptut list: 
+        - call_stats file
+        - vcf file
+        :return: None
+        """
+        self._tool_outputs['TXT_CALL_STATS'] = [
+            ToolIOFile(os.path.join(self._folder, self._parameters['output_callstats_file'].value))]
+        # self._tool_outputs['VCF'] = [
+        #     ToolIOFile(os.path.join(self._folder, self._parameters['output_vcf_file'].value))]
