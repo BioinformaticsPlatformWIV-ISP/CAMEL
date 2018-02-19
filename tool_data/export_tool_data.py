9ed55f78d90ece550b3f110d96cc1ae2d5ed3c58
Raf Winand <raf.winand@wiv-isp.be>
2018-02-19 13:41:45 +0100

    Added function to parse a YAML file with all tools and versions

--- tool_data/export_tool_data.py
+++ tool_data/export_tool_data.py
@@ -1,12 +1,13 @@
 import argparse
-
 import logging
+import sys
+from typing import Dict
+
 import os
 import yaml
 
 from app.camel import Camel
 from app.services.dbtoolservice import DbToolService
-
 
 """
 This scripts is used to export tool data from the database to a YAML file.
@@ -27,21 +28,22 @@ def _parse_arguments():
     :return: Arguments
     """
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('--tool-name', required=True)
-    argument_parser.add_argument('--tool-version', required=True)
+    argument_parser.add_argument('--tool-name', required=False)
+    argument_parser.add_argument('--tool-version', required=False)
+    argument_parser.add_argument('--tool-list', required=False)
     argument_parser.add_argument('--force', required=False, action='store_true')
     return argument_parser.parse_args()
 
 
-def _get_tool_data(camel: Camel, tool_name: str, version: str):
+def _get_tool_data(camel: Camel, tool_name: str, tool_version: str):
     """
     Returns the data for the given tool.
     :param camel: CAMEL instance
     :param tool_name: Tool name
-    :param version: Tool version
+    :param tool_version: Tool version
     :return: Tool data
     """
-    tool_service = DbToolService(tool_name, version, camel.connection)
+    tool_service = DbToolService(tool_name, tool_version, camel.connection)
 
     # Collect tool data
     tool_data = {
@@ -81,7 +83,24 @@ def export_tool_data(camel: Camel, tool_name: str, tool_version: str, force=Fals
         yaml.dump(tool_data, handle)
 
 
+def load_tool_list(tool_list_path: str) -> Dict[str, str]:
+    """
+    Loads the required tools and versions from the given YAML file.
+    :param tool_list_path: Path to the YAML file
+    :return: Dictionary with tool name as key and version as value
+    """
+    with open(tool_list_path) as handle:
+        return yaml.load(handle)
+
+
 if __name__ == '__main__':
     c = Camel()
     args = _parse_arguments()
-    export_tool_data(c, args.tool_name, args.tool_version, args.force)
+    if args.tool_name is not None and args.tool_list is not None:
+        logging.error('The arguments tool-name and tool-list are mutually exclusive!')
+        sys.exit(2)
+    if args.tool_name is not None:
+        export_tool_data(c, args.tool_name, args.tool_version, args.force)
+    if args.tool_list is not None:
+        for tool, version in load_tool_list(args.tool_list).items():
+            export_tool_data(c, tool, str(version), args.force)
