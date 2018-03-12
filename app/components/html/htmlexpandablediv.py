b7b6fcae485e636a80ff9d08c905c23199689da9
Bert Bogaerts <Bert.Bogaerts@wiv-isp.be>
2018-03-12 11:14:23 +0100

    Removed unused code, added warning

--- app/components/html/htmlexpandablediv.py
+++ app/components/html/htmlexpandablediv.py
@@ -3,13 +3,12 @@ from typing import List, Tuple
 from yattag import Doc
 
 from app.components.html.htmlelement import HtmlElement
-from app.components.html.htmlreport import HtmlReport
-from resources import CSS_STYLE
 
 
 class HtmlExpandableDiv(HtmlElement):
     """
     This class contains a div that can be shown / hidden by clicking.
+    The jquery source code needs to be included in the HTML report, otherwise this will not work!
     """
 
     SCRIPT_TEMPLATE = """
@@ -37,7 +36,6 @@ class HtmlExpandableDiv(HtmlElement):
         self._id = id_
         self._label = label
 
-    # noinspection PyArgumentList
     def to_html(self) -> str:
         """
         Converts this element to HTML code. A novel Doc() instance is created in order to nest the content of this
@@ -58,35 +56,3 @@ class HtmlExpandableDiv(HtmlElement):
             with tag('script', type='text/javascript'):
                     text(HtmlExpandableDiv.SCRIPT_TEMPLATE.format(id_=self._id))
         return doc.getvalue()
-
-
-if __name__ == '__main__':
-
-    report = HtmlReport('/scratch/bebog/working/divreport.html')
-    report.initialize('Test report', CSS_STYLE)
-    report.add_html_object(HtmlElement('script', '', attributes=[('type', 'text/javascript'),
-                                                                 ('src', 'jquery-3.2.1.min.js')]))
-    report.add_paragraph('This is a test file!')
-    report.add_html_object(HtmlElement('script', HtmlExpandableDiv.SCRIPT_TEMPLATE, [('type', 'text/javascript')]))
-    report.add_html_object(HtmlElement('p', ' Click to show / hide', [('id', 'toggle'), ('class', 'flip')]))
-    div_content = HtmlElement('div', attributes=[('id', 'content')])
-    div_content.add_paragraph('This is the div content')
-    report.add_html_object(div_content)
-    t = report.get_tag('test')
-    print(type(t))
-    report.save()
-
-"""
-script =
-$(document).ready(function(){
-$("#spoligo_toggle").click(function() {
-$("#spoligo_spacers").toggle(0, function() {});
-});
-$("#spoligo_spacers").hide()
-});
-\"""
-self._section.add_html_object(
-HtmlElement('script', '', attributes=[('type', 'text/javascript'), ('src', 'jquery-3.2.1.min.js')]))
-self._section.add_html_object(HtmlElement('script', script, [('type', 'text/javascript')]))
-        
-"""
