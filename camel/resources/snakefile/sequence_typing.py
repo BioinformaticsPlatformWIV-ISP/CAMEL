b9bd43d362705293a78c17125fba4969179ff2f4
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-08-09 16:58:18 +0200

    Added the Snakefile for the sequence typing workflow

---
+++ camel/resources/snakefile/sequence_typing.py
@@ -0,0 +1,6 @@
+import os
+
+OUTPUT_TYPING_REPORT = os.path.join('typing', '{scheme}', 'html.io')
+OUTPUT_TYPING_HITS = os.path.join('typing', '{scheme}', '{detection_method}', 'all-hits.io')
+OUTPUT_TYPING_REPORT_EMPTY = os.path.join('typing', '{scheme}', 'html-empty.io')
+OUTPUT_TYPING_SUMMARY = os.path.join('typing', '{scheme}', 'summary_out.tsv')
