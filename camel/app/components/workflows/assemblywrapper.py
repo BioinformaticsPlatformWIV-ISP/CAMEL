16a84961f476e19b2b16b38605d5a99ff96d40e5
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-09-25 12:03:25 +0200

    Added wrappers for the Snakefiles for assembly, gene detection and trimming

---
+++ camel/app/components/workflows/assemblywrapper.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+from typing import List, Optional, Dict, Any
+
+import os
+
+from camel.app.components.html.htmlreportsection import HtmlReportSection
+from camel.app.io.tooliofile import ToolIOFile
+from camel.app.snakemake.snakemakeutils import SnakemakeUtils
+from camel.app.snakemake.snakepipelineutils import SnakePipelineUtils
+from camel.resources.snakefile import SNAKEFILE_ASSEMBLY_SPADES
+from camel.resources.snakefile.assembly_spades import OUTPUT_ASSEMBLY_REPORT, OUTPUT_ASSEMBLY_FASTA
+from camel.resources.snakefile.read_trimming import OUTPUT_READ_TRIMMING_READS_PE, \
+    OUTPUT_READ_TRIMMING_READS_SE_REV, OUTPUT_READ_TRIMMING_READS_SE_FWD
+
+
+class AssemblyWrapper(object):
+    """
+    This class is used as a wrapper class around the assembly Snakemake workflow.
+    """
+
+    @dataclass
+    class AssemblyOutput:
+        report_section: HtmlReportSection
+        fasta_contigs: ToolIOFile
+
+    def __init__(self, working_dir: str) -> None:
+        """
+        Initializes the read trimming helper.
+        :param working_dir: Working directory
+        """
+        self._working_dir = working_dir
+        self._output = None
+
+    def run_workflow(self, sample_name: str, reads_pe: List[ToolIOFile], reads_se_fwd: List[ToolIOFile],
+                     reads_se_rev: List[ToolIOFile], kmers: str=None, threads: int=8) -> None:
+        """
+        Runs the read trimming workflow.
+        :param reads_pe: Paired end reads
+        :param reads_se_fwd: Single end forward reads
+        :param reads_se_rev: Single end reverse reads
+        :param sample_name: Sample name
+        :param kmers: Comma separated list of Kmer sizes to use for assembly
+        :param threads: Number of threads
+        :return: None
+        """
+        self.__prepare_input_files(reads_pe, reads_se_fwd, reads_se_rev)
+        config_data = self.__get_config_data(sample_name, kmers)
+        output_files = {
+            'HTML': os.path.join(self._working_dir, OUTPUT_ASSEMBLY_REPORT),
+            'FASTA': os.path.join(self._working_dir, OUTPUT_ASSEMBLY_FASTA)
+        }
+        SnakePipelineUtils.run_snakemake(
+            SNAKEFILE_ASSEMBLY_SPADES, config_data, list(output_files.values()), self._working_dir, threads)
+        self.__set_output(output_files)
+
+    def __get_config_data(self, sample_name: str, kmers: Optional[str]=None) -> Dict[str, Any]:
+        """
+        Builds the configuration file to run the assembly workflow.
+        :param sample_name: Sample name
+        :param kmers: Comma separated list of Kmer sizes to use for the assembly
+        :return: Config data
+        """
+        config_data = {'sample_name': sample_name, 'working_dir': self._working_dir}
+        if kmers is not None:
+            config_data['assembly'] = {'kmers': kmers}
+        return config_data
+
+    def __prepare_input_files(self, reads_pe: List[ToolIOFile], reads_se_fwd: List[ToolIOFile],
+                              reads_se_rev: List[ToolIOFile]) -> None:
+        """
+        Prepares the input files for the assembly workflow.
+        :return: None
+        """
+        dir_trimming_out = os.path.dirname(os.path.join(self._working_dir, OUTPUT_READ_TRIMMING_READS_PE))
+        if not os.path.isdir(dir_trimming_out):
+            os.makedirs(dir_trimming_out)
+        SnakemakeUtils.dump_object(reads_pe, os.path.join(self._working_dir, OUTPUT_READ_TRIMMING_READS_PE))
+        SnakemakeUtils.dump_object(reads_se_fwd, os.path.join(self._working_dir, OUTPUT_READ_TRIMMING_READS_SE_FWD))
+        SnakemakeUtils.dump_object(reads_se_rev, os.path.join(self._working_dir, OUTPUT_READ_TRIMMING_READS_SE_REV))
+
+    def __set_output(self, output_files: Dict[str, str]) -> None:
+        """
+        Runs the Snakemake workflow.
+        :param output_files: Output files dictionary
+        :return: None
+        """
+        self._output = AssemblyWrapper.AssemblyOutput(
+            report_section=SnakemakeUtils.load_object(output_files['HTML'])[0].value,
+            fasta_contigs=SnakemakeUtils.load_object(output_files['FASTA'])[0]
+        )
+
+    @property
+    def output(self) -> 'AssemblyWrapper.AssemblyOutput':
+        """
+        Returns the output of the assembly workflow.
+        :return: Assembly output
+        """
+        return self._output
