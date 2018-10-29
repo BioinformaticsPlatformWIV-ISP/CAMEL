5ceefe864cdab776166f5966ca47d5fbae7e5c63
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-10-29 17:23:51 +0100

    Updated main scripts for variant calling and variant filtering

---
+++ camel/scripts/variantcalling/samtools/mainfiltering.py
@@ -0,0 +1,80 @@
+import argparse
+from typing import Any, Dict
+
+import os
+
+import shutil
+
+from camel.app.camel import Camel
+from camel.app.components.workflows.variantfilteringwrapper import VariantFilteringWrapper
+
+
+class MainFiltering(object):
+    """
+    Class to run the samtools variant filtering using CAMEL.
+    """
+
+    def __init__(self, args: argparse.Namespace=None) -> None:
+        """
+        Initializes the main script.
+        """
+        self._args = args if args is not None else MainFiltering._parse_arguments()
+        self._camel = Camel()
+
+    @staticmethod
+    def _parse_arguments() -> argparse.Namespace:
+        """
+        Parses the command line arguments.
+        :return: Arguments
+        """
+        argument_parser = argparse.ArgumentParser()
+        argument_parser.add_argument('--vcf', required=True, help="Input VCF file")
+        argument_parser.add_argument('--bam')
+        argument_parser.add_argument('--output-vcf', required=True)
+        argument_parser.add_argument('--working-dir', default=os.path.abspath('.'))
+        argument_parser.add_argument('--min-total-depth', default=10, type=int)
+        argument_parser.add_argument('--min-forward-depth', default=1, type=int)
+        argument_parser.add_argument('--min-reverse-depth', default=1, type=int)
+        argument_parser.add_argument('--min-snp-quality', default=25, type=float)
+        argument_parser.add_argument('--min-mapping-quality', default=30, type=int)
+        argument_parser.add_argument('--min-distance', default=10, type=int)
+        argument_parser.add_argument('--keep-best', action='store_true')
+        argument_parser.add_argument('--min-zscore', default=1.96, type=float)
+        argument_parser.add_argument('--y-mult', default=10, type=float)
+        return argument_parser.parse_args()
+
+    def run(self) -> None:
+        """
+        Filter the input VCF file.
+        :return: None
+        """
+        wrapper = VariantFilteringWrapper(self._args.working_dir)
+        wrapper.run_workflow(self._args.vcf, self._args.bam, filtering_options=self.__get_filtering_options())
+        shutil.copyfile(wrapper.output.vcf_filtered.path, self._args.output_vcf)
+
+    def __get_filtering_options(self) -> Dict[str, Any]:
+        """
+        Returns the dictionary with filtering options.
+        :return: Filtering options
+        """
+        return {
+            'depth': {
+                'min_total_depth': self._args.min_total_depth,
+                'min_fwd_depth': self._args.min_forward_depth,
+                'min_rev_depth': self._args.min_reverse_depth},
+            'snp_quality': {
+                'min_snp_quality': self._args.min_snp_quality},
+            'mapping_quality': {
+                'min_mapping_quality': self._args.min_mapping_quality},
+            'distance': {
+                'min_distance': self._args.min_distance,
+                'keep_best': self._args.keep_best},
+            'zscore': {
+                'min_zscore': self._args.min_zscore,
+                'y_multiplier': self._args.y_mult}
+        }
+
+
+if __name__ == '__main__':
+    main = MainFiltering()
+    main.run()
