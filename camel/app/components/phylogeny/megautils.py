a169837f22fea04e3907bf48d24d068687a8b23e
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-11-06 16:41:43 +0100

    Updated the methods to update the mega parameters

--- camel/app/components/phylogeny/megautils.py
+++ camel/app/components/phylogeny/megautils.py
@@ -9,13 +9,14 @@ class MEGAUtils(object):
 
     @staticmethod
     def update_model_selection_parameters(model_selection: ModelSelection, missing_data: str, branch_swap: str,
-                                          site_cov_cutoff: int) -> None:
+                                          site_cov_cutoff: int, threads: int) -> None:
         """
         Utility function to set the parameters for the ModelSelection tool.
         :param model_selection: Model selection tool instance
         :param missing_data: Missing data parameter value
         :param branch_swap: Branch swap parameter value
         :param site_cov_cutoff: Site coverage cutoff
+        :param threads: Number of threads to use
         :return: None
         """
         if missing_data == 'complete_deletion':
@@ -27,12 +28,12 @@ class MEGAUtils(object):
                 missing_data_treatment='Partial deletion', site_coverage_cutoff=site_cov_cutoff)
         else:
             raise ValueError(f"Invalid parameter value for missing data: '{missing_data}'")
-        model_selection.update_parameters(branch_swap_filter=branch_swap.title().replace('_', ' '))
+        model_selection.update_parameters(branch_swap_filter=branch_swap.title().replace('_', ' '), threads=threads)
 
     @staticmethod
     def update_tree_building_parameters(tree_building: MLTreeConstruction, model: str, rates: str, bootstraps: int,
                                         missing_data: str, site_cov_cutoff: int, ml_method: str,
-                                        branch_swap: str) -> None:
+                                        branch_swap: str, threads: int) -> None:
         """
         Updates the parameters of the tree building tool.
         :param tree_building: Tree building tool instance
@@ -43,13 +44,15 @@ class MEGAUtils(object):
         :param model: Selection model
         :param ml_method: ML method (e.g. SPR3)
         :param branch_swap: Branch swap filter
+        :param threads: Number of treads to use
         :return: None
         """
         tree_building.update_parameters(
             bootstrap_replications=bootstraps, test_of_phylogeny='Bootstrap method',
             model=model,
             branch_swap_filter=branch_swap.title().replace('_', ' '),
-            heuristic_method=ml_method.upper()
+            heuristic_method=ml_method.upper(),
+            threads=threads
         )
 
         # Add rates parameter
