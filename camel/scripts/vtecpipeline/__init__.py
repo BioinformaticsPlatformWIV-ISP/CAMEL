81e5d15077cdb26ded9b13f1131f759a790f8255
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-11-21 09:41:26 +0100

    Removed unused VTEC pipeline code

--- camel/scripts/vtecpipeline/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-
-_current_dir = os.path.abspath(os.path.dirname(__file__))
-
-SNAKEFILE_VTEC_MAIN = os.path.join(_current_dir, 'main.snakefile')
--- camel/scripts/vtecpipeline/subworkflows/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-__CURRENT_DIR = os.path.dirname(__file__)
-
-WORKFLOW_ASSEMBLY = os.path.join(__CURRENT_DIR, 'assembly.snakefile')
-WORKFLOW_GENE_DETECTION = os.path.join(__CURRENT_DIR, 'gene_detection.snakefile')
-WORKFLOW_READ_TRIMMING = os.path.join(__CURRENT_DIR, 'read_trimming.snakefile')
