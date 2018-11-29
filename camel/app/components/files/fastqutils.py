7e38af91d86c3b153beae3937ac2d7588290808e
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2018-11-29 15:27:46 +0100

    The get sample name function now accepts gzipped fastq files

--- camel/app/components/files/fastqutils.py
+++ camel/app/components/files/fastqutils.py
@@ -209,17 +209,13 @@ class FastqUtils(object):
         Returns the sample name based on the given reads. It tries to match the following formats(in this order):
         - Sample - Name_S\d + _L\d + _R[12]_\d + .fastq(e.g.: S15BD00757_S20_L001_R2_001.fastq)
         - Sample - Name_1.fastq, Sample - Name_2.fastq(e.g.: reads_1.fastq)
-        :
-            param fastq_filename:
-                FASTQ filename
-        :
-            return:
-                Sample name
+        :param fastq_filename: FASTQ filename
+        :return: Sample name
         """
-        m = re.match('(.*)_S\d+_L\d+_R[12]_\d+.[fastq]+$', fastq_filename)
+        m = re.match('(.*)_S\d+_L\d+_R[12]_\d+.[fastq]+(.gz)?$', fastq_filename)
         if m:
             return m.group(1)
-        m = re.match('(.*)_[12].[fastq]+$', fastq_filename)
+        m = re.match('(.*)_[12].[fastq]+(.gz)?$', fastq_filename)
         if m:
             return m.group(1)
         raise ValueError("Cannot determine sample name from: {}".format(fastq_filename))
