717f622bf83e77ad08444a22f8a685e53a4b13be
Tdelcourt <thomas.delcourt@wiv-isp.be>
2019-01-23 11:42:37 +0100

    Bugfix: change tool_data files gatk (java-jdk now java)

--- camel/scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.sh
+++ camel/scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.sh
@@ -2,11 +2,12 @@
 echo 'running GATK somatic variant calling pipeline'
 echo $@
 # for testing
-#. /home/todel/PycharmProjects/camel_3.0/camel_3_env/bin/activate
-#export PYTHONPATH=/home/todel/PycharmProjects/camel_3.0/
-#python3 /home/todel/PycharmProjects/camel_3.0/camel/scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.py "$@"  --wdir $PWD
+# . /home/todel/PycharmProjects/camel_3.0/camel_3_env/bin/activate
+# export PYTHONPATH=/home/todel/PycharmProjects/camel_3.0/
+# python3 /home/todel/PycharmProjects/camel_3.0/camel/scripts/gatksomaticvariantcalling/run_gatk_somatic_pipeline.py "$@"  --wdir $PWD
 
 # for real running with lmod
+ml load java/1.8.0_172
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( dirname "$SOURCE" )"
 python3 $DIR/run_gatk_somatic_pipeline.py "$@"
