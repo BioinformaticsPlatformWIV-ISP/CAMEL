55c719f599a0fa145f28d9a7886421775d15be33
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2019-01-30 14:31:51 +0100

    Added tools for the serogroup determination of the Neisseria pipeline

---
+++ camel/app/tools/pipelines/neisseria/serogroupdetermination.py
@@ -0,0 +1,67 @@
+from typing import List
+
+from enum import Enum
+
+from camel.app.camel import Camel
+from camel.app.components.sequencetyping.sequencetypingblasthit import SequenceTypingBlastHit
+from camel.app.tools.tool import Tool
+
+
+class DetectionCategory(Enum):
+    ALL_PERFECT = 1
+    IMPERFECT_IDENTITY = 2
+    IMPERFECT_ALL = 3
+    OTHER = 4
+
+
+class SerogroupDetermination(Tool):
+    """
+    This tool is used to determine the serogroup of Neisseria meningitidis isolates.
+    """
+
+    def __init__(self, camel: Camel) -> None:
+        """
+        Initializes this tool.
+        :param camel: CAMEL instance
+        """
+        super().__init__('Neisseria: serogroup determination', '0.1', camel)
+
+    def _execute_tool(self) -> None:
+        """
+        Executes this tool.
+        :return: None
+        """
+        serogroup_stats = []
+        for key in self._tool_inputs:
+            # Extract hits and sort them
+            hits = [h.value for h in self._tool_inputs[key]]
+            hits.sort(key=lambda h: h.locus)
+
+            # Determine the number of perfect hits
+            nb_perfect_hits = len([h for h in hits if h.is_perfect_hit()])
+            serogroup_stats.append({
+                'name': key.split('_')[-1].upper(),
+                'category': self.__get_detection_category(hits).value,
+                'nb_loci_total': len(hits),
+                'nb_perfect': nb_perfect_hits,
+                'fraction_perfect': nb_perfect_hits / len(hits),
+                'color_per_hit': [(h.locus, h.color) for h in hits]
+            })
+
+        # Sort serogroups based on category and fraction of perfect hits
+        serogroup_stats.sort(key=lambda x: (x['category'], -x['fraction_perfect']))
+        self._informs['serogroups_sorted'] = serogroup_stats
+
+    def __get_detection_category(self, hits: List[SequenceTypingBlastHit]) -> DetectionCategory:
+        """
+        Returns the category of hits for the given list of hits.
+        :param hits: Hits
+        :return: Detection type
+        """
+        if all(h.is_perfect_hit() for h in hits):
+            return DetectionCategory.ALL_PERFECT
+        elif all(h.subject_coverage == 100.0 for h in hits):
+            return DetectionCategory.IMPERFECT_IDENTITY
+        elif not any(h.allele_id == '-' for h in hits):
+            return DetectionCategory.IMPERFECT_ALL
+        return DetectionCategory.OTHER
---
+++ camel/app/tools/pipelines/neisseria/serogroupdeterminationreporter.py
@@ -0,0 +1,47 @@
+from camel.app.camel import Camel
+from camel.app.components.html.htmlreportsection import HtmlReportSection
+from camel.app.components.html.htmltablecell import HtmlTableCell
+from camel.app.io.tooliovalue import ToolIOValue
+from camel.app.tools.tool import Tool
+
+
+class SerogroupDeterminationReporter(Tool):
+    """
+    This class is used to generate an output report for the serogroup determination.
+    """
+
+    def __init__(self, camel: Camel) -> None:
+        """
+        Initializes this tool.
+        :param camel: CAMEL instance
+        """
+        super().__init__('Neisseria: serogroup determination reporter', '0.1', camel)
+        self._section = HtmlReportSection('Serogroup determination')
+
+    def _execute_tool(self) -> None:
+        """
+        Executes this tool.
+        :return: None
+        """
+        detected_serogroup = self._input_informs['analysis']['serogroups_sorted'][0]['name']
+        self._section.add_paragraph(f'Detected serogroup: <b>{detected_serogroup}</b>')
+        self.__add_table_detected_loci()
+        self._tool_outputs['VAL_HTML'] = [ToolIOValue(self._section)]
+    
+    def __add_table_detected_loci(self) -> None:
+        """
+        Adds the table with the detected loci.
+        :return: None
+        """
+        table_data = []
+        for s in self._input_informs['analysis']['serogroups_sorted']:
+            row = [s['name'], '{:.2f}'.format(100 * s['fraction_perfect'])]
+            for i in range(0, 7):
+                try:
+                    locus_name, color = s['color_per_hit'][i]
+                    row.append(HtmlTableCell(locus_name, color))
+                except IndexError:
+                    row.append('-')
+            table_data.append(row)
+        header = ['Serogroup', 'Genes detected (%)'] + [''] * 7
+        self._section.add_table(table_data, header, [('class', 'data')])
