809a64b5b2219e1063481a6580a1442ad5c0781b
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2019-04-17 15:17:41 +0200

    Implemented a base class for the isolate pipelines

--- camel/scripts/neisseriapipeline/__init__.py
+++ camel/scripts/neisseriapipeline/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 _current_dir = os.path.dirname(__file__)
+CONFIG_DATA = os.path.join(_current_dir, 'config', 'config_data.yml')
+CITATIONS_HTML = os.path.join(_current_dir, 'citations.html')
 SNAKEFILE_MAIN = os.path.join(_current_dir, 'snakefile', 'main.smk')
 SNAKEFILE_SEROGROUP_DETERMINATION = os.path.join(_current_dir, 'snakefile', 'serogroup_determination.smk')
-CONFIG_TEMPLATE = os.path.join(_current_dir, 'config', 'config_template.yml')
-CITATIONS_HTML = os.path.join(_current_dir, 'citations.html')
