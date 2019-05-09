1c05a46d48ee46015224e34abf67ffac3918f5da
Bert Bogaerts <bert.bogaerts@wiv-isp.be>
2019-05-09 14:12:57 +0200

    Updated main script for the STEC pipeline

--- camel/scripts/stecpipeline/__init__.py
+++ camel/scripts/stecpipeline/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 _current_dir = os.path.dirname(__file__)
+CITATIONS_HTML = os.path.join(_current_dir, 'citations.html')
+CONFIG_TEMPLATE = os.path.join(_current_dir, 'config', 'config_template.yml')
+CONFIG_DATA = os.path.join(_current_dir, 'config', 'config_data.yml')
 SNAKEFILE_MAIN = os.path.join(_current_dir, 'snakefile', 'main.smk')
 SNAKEFILE_SEROTYPE = os.path.join(_current_dir, 'snakefile', 'serotype_detection.smk')
-CONFIG_TEMPLATE = os.path.join(_current_dir, 'config', 'config_template.yml')
-CITATIONS_HTML = os.path.join(_current_dir, 'citations.html')
