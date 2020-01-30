8f88b9520d9944dde361bdd0976544ac5c61e01a
Bert Bogaerts <bert.bogaerts@sciensano.be>
2020-01-30 14:34:11 +0100

    Switched to pathlib, added method to create pipeline html report output. Added helper function to extract FASTQ output for various workflows

--- camel/app/snakemake/snakepipelineutils.py
+++ camel/app/snakemake/snakepipelineutils.py
@@ -1,14 +1,16 @@
 import logging
-from typing import List, Tuple, Any, Dict
+from pathlib import Path
+from typing import List, Tuple, Any, Dict, Optional
 
-import os
 import yaml
 
 from camel.app.command.command import Command
 from camel.app.components.filesystemhelper import FileSystemHelper
 from camel.app.components.html.htmlelement import HtmlElement
+from camel.app.components.html.htmlreport import HtmlReport
 from camel.app.components.html.htmlreportsection import HtmlReportSection
 from camel.app.error.snakemakeexecutionerror import SnakemakeExecutionError
+from camel.app.io.tooliofile import ToolIOFile
 from camel.app.io.tooliovalue import ToolIOValue
 from camel.app.snakemake.snakemakeutils import SnakemakeUtils
 
@@ -45,20 +47,33 @@ class SnakePipelineUtils(object):
         return section
 
     @staticmethod
-    def create_sections_overview(report_sections: List[Tuple[str, str, List[str]]]) -> HtmlReportSection:
+    def add_report_content(report: HtmlReport, report_structure: List[Tuple[str, str, List[str]]]) -> None:
         """
-        Creates the sections overview for the HTML report.
-        :param report_sections: Overview of the report sections (name, abbreviation, path)
-        :return: HTML report section
+        Adds the content to the HTML report.
+        :param report: Report
+        :param report_structure: Report structure
+        :return: None
         """
+        # Add the overview section
         section = HtmlReportSection(None)
         overview_list = HtmlElement('ul')
-        for title, key, _ in report_sections:
+        for title, key, _ in report_structure:
             list_item = HtmlElement('li')
             list_item.add_html_object(HtmlElement('a', title, [('href', '#{}'.format(key))]))
             overview_list.add_html_object(list_item)
         section.add_html_object(overview_list)
-        return section
+        report.add_html_object(section)
+
+        # Add the different sections
+        for title, key, items in report_structure:
+            report.add_module_header(title, key)
+            for pickle in items:
+                if len(pickle) == 0:
+                    continue
+                section = SnakemakeUtils.load_object(pickle)[0].value
+                report.add_html_object(section)
+                section.copy_files(report.output_dir)
+        report.save()
 
     @staticmethod
     def create_empty_report_section(title: str, output_file: str, header_level: int = 3) -> None:
@@ -74,8 +89,8 @@ class SnakePipelineUtils(object):
         SnakemakeUtils.dump_object([ToolIOValue(section)], output_file)
 
     @staticmethod
-    def symlink_input_files(output_dir: str, file_paths: List[str], file_names: List[str], sanitize: bool = False) ->\
-            List[str]:
+    def symlink_input_files(output_dir: Path, file_paths: List[str], file_names: List[str], sanitize: bool = False) ->\
+            List[Path]:
         """
         Creates symlinks with the given names for the given files.
         This can be used for files that come from Galaxy that have a fixed name (dataset_XXXXX.dat).
@@ -88,19 +103,19 @@ class SnakePipelineUtils(object):
         if len(file_names) != len(file_paths):
             raise ValueError("File names ({}) and file paths ({}) should be the same length".format(
                 len(file_names), len(file_paths)))
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir(parents=True)
         links = []
         for path, name in zip(file_paths, file_names):
-            link_path = os.path.join(output_dir, FileSystemHelper.make_valid(name) if sanitize else name)
-            if os.path.islink(link_path):
-                os.remove(link_path)
-            os.symlink(path, link_path)
+            link_path = output_dir / (FileSystemHelper.make_valid(name) if sanitize else name)
+            if link_path.is_symlink():
+                link_path.unlink()
+            link_path.symlink_to(path)
             links.append(link_path)
         return links
 
     @staticmethod
-    def generate_config_file(config_data: Dict[str, Any], output_dir: str, output_basename: str = 'config.yml') -> str:
+    def generate_config_file(config_data: Dict[str, Any], output_dir: Path, output_basename: str = 'config.yml') -> str:
         """
         Generates a configuration file for Snakemake in YAML file format.
         :param config_data: Configuration data
@@ -108,16 +123,16 @@ class SnakePipelineUtils(object):
         :param output_basename: Output basename
         :return: Path to config file
         """
-        config_path = os.path.join(output_dir, output_basename)
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
-        with open(config_path, 'w') as handle:
+        config_path = output_dir / output_basename
+        if not output_dir.exists():
+            output_dir.mkdir(parents=True)
+        with config_path.open('w') as handle:
             yaml.dump(config_data, handle)
         logging.info(f"Configuration file created: {config_path}")
-        return config_path
+        return str(config_path)
 
     @staticmethod
-    def run_snakemake(snakefile: str, config_path: str, targets: List[str], working_dir: str,
+    def run_snakemake(snakefile: str, config_path: str, targets: List[Path], working_dir: Path,
                       threads: int = 8) -> None:
         """
         Helper function to run snakemake workflows.
@@ -128,11 +143,11 @@ class SnakePipelineUtils(object):
         :param threads: Number of threads to use
         :return: None
         """
-        if not os.path.isdir(working_dir):
-            os.makedirs(working_dir)
+        if not working_dir.exists():
+            working_dir.mkdir(parents=True)
         command = Command('snakemake {} --snakefile {} --configfile {} --cores {}'.format(
-            ' '.join(targets), snakefile, config_path, threads))
-        command.run_command(working_dir)
+            ' '.join(str(x) for x in targets), snakefile, config_path, threads))
+        command.run_command(str(working_dir))
         print(f'- Stdout: -\n{command.stdout}')
         print(f'- Stderr: -\n{command.stderr}')
         if command.returncode != 0:
@@ -154,3 +169,24 @@ class SnakePipelineUtils(object):
             command_txt = informs['_command'].replace(working_dir, '$WORKING')
             section.add_html_object(HtmlElement('code', command_txt, [('class', 'command')]))
         return section
+
+    @staticmethod
+    def extracts_fq_input(io_dict: str, key_pe: Optional[str] = 'FASTQ_PE', key_se: Optional[str] = 'FASTQ_SE',
+                          drop_se: bool = False) -> Dict[str, List[ToolIOFile]]:
+        """
+        Extracts a specific FASTQ input dictionary from the standardized FASTQ dictionary.
+        :param io_dict: Path to input IO file
+        :param key_pe: Key for paired end FASTQ files
+        :param key_se: Key for single end FASTQ files
+        :param drop_se: If True, SE reads are dropped for PE input
+        :return: Reformatted dictionary
+        """
+        io = SnakemakeUtils.load_object(io_dict)
+        if 'PE' in io:
+            output_dict = {key_pe: io['PE']}
+            se_reads = io.get('SE_FWD', []) + io.get('SE_REV', [])
+            if len(se_reads) > 0 and not drop_se:
+                output_dict[key_se] = se_reads
+        else:
+            output_dict = {key_se: io['SE']}
+        return output_dict
