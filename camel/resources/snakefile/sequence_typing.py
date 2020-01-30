1cfd7aed45e7ddc0a4b541a7282019782f105c2e
Bert Bogaerts <bert.bogaerts@sciensano.be>
2020-01-30 16:07:15 +0100

    Added updated wrapper script for sequence typing workflow

--- camel/resources/snakefile/sequence_typing.py
+++ camel/resources/snakefile/sequence_typing.py
@@ -1,11 +1,16 @@
-from typing import Dict, Any, Optional
+from pathlib import Path
+from typing import Dict, Any, Optional, List, Union
 
-import os
+SNAKEFILE_SEQUENCE_TYPING = f'{Path(__file__).parent / Path(__file__).stem}.smk'
+SNAKEFILE_SEQUENCE_TYPING_BLAST = f'{Path(__file__).parent / Path(__file__).stem}_blast.smk'
+SNAKEFILE_SEQUENCE_TYPING_SRST2 = f'{Path(__file__).parent / Path(__file__).stem}_srst2.smk'
+SNAKEFILE_SEQUENCE_TYPING_KMA = f'{Path(__file__).parent / Path(__file__).stem}_kma.smk'
 
-OUTPUT_TYPING_REPORT = os.path.join('typing', '{scheme}', 'html.io')
-OUTPUT_TYPING_HITS = os.path.join('typing', '{scheme}', '{locus_type}', '{detection_method}', 'all-hits.io')
-OUTPUT_TYPING_REPORT_EMPTY = os.path.join('typing', '{scheme}', 'html-empty.io')
-OUTPUT_TYPING_SUMMARY = os.path.join('typing', '{scheme}', 'summary_out.tsv')
+_dir_typing = Path('typing', '{scheme}')
+OUTPUT_TYPING_REPORT = _dir_typing / 'html.io'
+OUTPUT_TYPING_REPORT_EMPTY = _dir_typing / 'html-empty.io'
+OUTPUT_TYPING_HITS = _dir_typing / '{locus_type}' / '{detection_method}' / 'all-hits.io'
+OUTPUT_TYPING_SUMMARY = _dir_typing / 'summary_out.tsv'
 
 
 def get_sequence_typing_report(scheme_key: str, config: Dict[str, Any], analysis_name: Optional[str] = None) -> str:
@@ -18,5 +23,5 @@ def get_sequence_typing_report(scheme_key: str, config: Dict[str, Any], analysis
     """
     search_key = analysis_name if analysis_name is not None else scheme_key
     if search_key not in config['analyses']:
-        return os.path.join(config['working_dir'], OUTPUT_TYPING_REPORT_EMPTY.format(scheme=scheme_key))
-    return os.path.join(config['working_dir'], OUTPUT_TYPING_REPORT.format(scheme=scheme_key))
+        return Path(config['working_dir']) / str(OUTPUT_TYPING_REPORT_EMPTY).format(scheme=scheme_key)
+    return Path(config['working_dir']) / str(OUTPUT_TYPING_REPORT).format(scheme=scheme_key)
