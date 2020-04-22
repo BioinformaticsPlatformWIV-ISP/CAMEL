1d8afc321aec5e35dd36b7d98c88afa828fd02f7
bert.bogaerts@sciensano.be <yllwPND2>
2020-04-22 16:41:30 +0200

    Removed unused template file

--- camel/scripts/shigellapipeline/__init__.py
+++ camel/scripts/shigellapipeline/__init__.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 _current_dir = Path(__file__).parent
 CITATIONS_HTML = str(_current_dir / 'citations.html')
-CONFIG_TEMPLATE = str(_current_dir / 'config' / 'config_template.yml')
 CONFIG_DATA = str(_current_dir / 'config' / 'config_data.yml')
 SNAKEFILE_MAIN = str(_current_dir / 'snakefile' / 'main.smk')
 SNAKEFILE_SUBSPECIES = str(_current_dir / 'snakefile' / 'subspecies_identification.smk')
--- camel/scripts/stecpipeline/__init__.py
+++ camel/scripts/stecpipeline/__init__.py
@@ -2,6 +2,5 @@ import os
 
 _current_dir = os.path.dirname(__file__)
 CITATIONS_HTML = os.path.join(_current_dir, 'citations.html')
-CONFIG_TEMPLATE = os.path.join(_current_dir, 'config', 'config_template.yml')
 CONFIG_DATA = os.path.join(_current_dir, 'config', 'config_data.yml')
 SNAKEFILE_MAIN = os.path.join(_current_dir, 'snakefile', 'main.smk')
