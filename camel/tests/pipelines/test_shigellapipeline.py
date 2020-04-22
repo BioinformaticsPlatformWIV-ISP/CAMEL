ab64a21ba95f316f3a37649bab8c0e090c40c3ff
bert.bogaerts@sciensano.be <yllwPND2>
2020-04-22 15:36:44 +0200

    Updated tests for shigella pipeline

--- camel/tests/pipelines/test_shigellapipeline.py
+++ camel/tests/pipelines/test_shigellapipeline.py
@@ -1,37 +1,27 @@
 import unittest
 from pathlib import Path
 
-import tempfile
 import yaml
 
 from camel.app.camel import Camel
+from camel.app.components.testing.cameltestsuite import CamelTestSuite
 from camel.app.io.tooliodirectory import ToolIODirectory
 from camel.scripts.shigellapipeline import CONFIG_DATA
 from camel.scripts.shigellapipeline.mainshigellapipeline import MainShigellaPipeline
 from camel.tests import longRunningTest
 
 
-class TestShigellaPipeline(unittest.TestCase):
+class TestShigellaPipeline(CamelTestSuite):
     """
     Tests for the Shigella pipeline.
     """
 
-    camel = Camel()
-    running_dir = None
-
     # Input files
-    test_file_dir = Path(camel.config['testing']['testfiles_dir'])
+    test_file_dir = CamelTestSuite.get_test_file_dir('pipelines')
     input_fastq_pe = [
-        test_file_dir / 'pipelines' / 'Shigella-S17BD07654_1.fastq.gz',
-        test_file_dir / 'pipelines' / 'Shigella-S17BD07654_2.fastq.gz'
+        test_file_dir / 'Shigella-S17BD07654_1.fastq.gz',
+        test_file_dir / 'Shigella-S17BD07654_2.fastq.gz'
     ]
-
-    def setUp(self):
-        """
-        Sets up the resources before running the test.
-        :return: None
-        """
-        self.running_dir = Path(tempfile.mkdtemp(None, 'camel_', TestShigellaPipeline.camel.config['temp_dir']))
 
     def test_shigella_pipeline_typing_db(self) -> None:
         """
@@ -42,13 +32,13 @@ class TestShigellaPipeline(unittest.TestCase):
         with open(CONFIG_DATA) as handle_in:
             config_data = yaml.safe_load(handle_in)
 
-        for key, path in config_data['sequence_typing'].items():
+        for key, scheme_data in config_data['sequence_typing'].items():
             # Check if scheme exists
-            self.assertGreater(Path(path).stat().st_size, 0)
+            self.assertGreater(Path(scheme_data['path']).stat().st_size, 0)
 
             # Check if metadata can be loaded
             manager = LocusSetManager(Camel.get_instance())
-            manager.add_input_files({'DIR': [ToolIODirectory(path)]})
+            manager.add_input_files({'DIR': [ToolIODirectory(scheme_data['path'])]})
             manager.run(str(self.running_dir))
             self.assertGreater(len(manager.informs), 0)
 
@@ -73,7 +63,7 @@ class TestShigellaPipeline(unittest.TestCase):
             self.assertGreater(len(manager.informs), 0)
 
     @longRunningTest()
-    def test_shigella_pipeline(self) -> None:
+    def test_shigella_pipeline_blast(self) -> None:
         """
         Tests the Shigella pipeline with all assays except for cgMLST.
         :return: None
@@ -86,6 +76,26 @@ class TestShigellaPipeline(unittest.TestCase):
             '--output-dir', str(path_report_out.parent),
             '--output-tsv', str(path_summary_out),
             '--working-dir', str(self.running_dir)
+        ] + [f"--{a.replace('_', '-')}" for a in MainShigellaPipeline.CUSTOM_ANALYSES if a != 'cgmlst']
+        main = MainShigellaPipeline(args)
+        main.run()
+        self.assertGreater(path_report_out.stat().st_size, 0)
+
+    @longRunningTest()
+    def test_shigella_pipeline_srst2(self) -> None:
+        """
+        Tests the Shigella pipeline with all assays except for cgMLST.
+        :return: None
+        """
+        path_report_out = self.running_dir / 'out' / 'report.html'
+        path_summary_out = self.running_dir / 'out' / 'summary.tsv'
+        args = [
+            '--fastq-pe', str(TestShigellaPipeline.input_fastq_pe[0]), str(TestShigellaPipeline.input_fastq_pe[1]),
+            '--output-html', str(path_report_out),
+            '--output-dir', str(path_report_out.parent),
+            '--output-tsv', str(path_summary_out),
+            '--working-dir', str(self.running_dir),
+            '--detection-method', 'srst2'
         ] + [f"--{a.replace('_', '-')}" for a in MainShigellaPipeline.CUSTOM_ANALYSES if a != 'cgmlst']
         main = MainShigellaPipeline(args)
         main.run()
