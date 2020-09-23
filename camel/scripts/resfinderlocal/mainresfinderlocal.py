8199adad299d6b8f73c121d154f8217d1d0c9983
bert.bogaerts@sciensano.be <yllwPND2>
2020-09-23 17:13:25 +0200

    Fixed a bug where parameters where not correctly set for resfinder local

--- camel/scripts/resfinderlocal/mainresfinderlocal.py
+++ camel/scripts/resfinderlocal/mainresfinderlocal.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 import argparse
-import datetime
 import logging
+from pathlib import Path
 from typing import Any, Dict, Optional, Sequence
 
 import os
 
+from camel.app.components import mainscriptutils
 from camel.app.components.html.htmlreport import HtmlReport
-from camel.app.components.html.htmlreportsection import HtmlReportSection
-from camel.app.components.mainscripthelper import MainScriptHelper
-from camel.app.components.workflows.genedetectionwrapper import GeneDetectionWrapper
-from camel.app.io.tooliofile import ToolIOFile
+from camel.app.components.workflows.genedetectionwrapper import GeneDetectionWrapper, GeneDetectionOutput
+from camel.app.components.workflows.readtype import helper_by_read_type
 from camel.app.snakemake.snakepipelineutils import SnakePipelineUtils
-from camel.resources import CSS_STYLE
 
 
 class MainResFinderLocal(object):
@@ -26,9 +24,8 @@ class MainResFinderLocal(object):
         :param args: Arguments (optional)
         """
         self._args = MainResFinderLocal.parse_arguments(args)
-        self._sample_name = MainScriptHelper.determine_sample_name(self._args)
-        self._helper = MainScriptHelper(self._args.working_dir, self._sample_name)
-        self._report = None
+        self._sample_name = mainscriptutils.determine_sample_name(self._args)
+        self._helper = helper_by_read_type[self._args.read_type](Path(self._args.working_dir), self._sample_name)
 
     @staticmethod
     def parse_arguments(args: Optional[Sequence[str]] = None) -> argparse.Namespace:
@@ -37,17 +34,12 @@ class MainResFinderLocal(object):
         :return: Parsed arguments
         """
         argument_parser = argparse.ArgumentParser()
-        MainScriptHelper.add_common_arguments(argument_parser)
-        argument_parser.add_argument('--fasta', help="Input FASTA file")
-        argument_parser.add_argument('--fasta-name', help="Input FASTA file name")
-        argument_parser.add_argument('--fastq-pe', help="Input PE FASTQ files", nargs=2)
-        argument_parser.add_argument('--fastq-pe-names', help="Input PE FASTQ file names", nargs=2)
-        argument_parser.add_argument('--trim-reads', help="Perform read trimming", action='store_true')
-        argument_parser.add_argument('--kmers', help="Kmers to use for assembly")
+        mainscriptutils.add_common_arguments(argument_parser)
+        mainscriptutils.add_input_files_arguments(argument_parser)
+        mainscriptutils.add_assembly_arguments(argument_parser)
         argument_parser.add_argument('--min-percent-identity', type=int, default=90)
         argument_parser.add_argument('--min-percent-coverage', type=int, default=60)
         argument_parser.add_argument('--resfinder-db', type=str, required=True)
-        argument_parser.add_argument('--report-include-fastq', action='store_true')
         return argument_parser.parse_args(args)
 
     def run(self) -> None:
@@ -55,38 +47,22 @@ class MainResFinderLocal(object):
         Runs the main script.
         :return: None
         """
-        self.__init_report()
-        self.__add_analysis_info_section()
-        input_files = self._helper.symlink_input_files(self._args.fasta, self._args.fastq_pe)
-        fasta_file = self._helper.get_blast_input(input_files, self._args, self._report)
+        # Initialize report
+        report = mainscriptutils.init_report(
+            Path(self._args.output_html), Path(self._args.output_dir), 'ResFinder local report', 'ResFinder local')
+        report.add_html_object(mainscriptutils.generate_analysis_info_section(self._args))
+        report.save()
+
+        # Run tools
+        fasta_file = self._helper.prepare_fasta_input(report, self._args)
         db_data = self.__get_db_data()
-        self.__run_gene_detection(fasta_file, db_data)
+        wrapper = self.__run_gene_detection(fasta_file, db_data, report)
 
-    def __init_report(self) -> None:
-        """
-        Initializes the HTML report
-        :return: None
-        """
-        self._report = HtmlReport(self._args.output_html, self._args.output_dir)
-        if not os.path.isdir(self._args.output_dir):
-            os.makedirs(self._args.output_dir)
-        self._report.initialize('ResFinder local report', CSS_STYLE)
-        self._report.add_pipeline_header('ResFinder local')
-
-    def __add_analysis_info_section(self) -> None:
-        """
-        Adds the report section with the analysis info
-        :return: None
-        """
-        section = HtmlReportSection('Analysis info')
-        section.add_table([
-            ['Analysis date:', datetime.datetime.now().strftime(SnakePipelineUtils.DATE_FORMAT)],
-            ['Input file(s):', self._helper.determine_input_files(self._args)],
-            ['Selected % identity threshold:', f'{self._args.min_percent_identity}%'],
-            ['Selected % query covered threshold:', f'{self._args.min_percent_coverage}%']
-        ], table_attributes=[('class', 'information')])
-        self._report.add_html_object(section)
-        self._report.save()
+        # Save report
+        all_informs = self._helper.informs + [wrapper.informs]
+        report.add_html_object(SnakePipelineUtils.create_commands_section(all_informs, self._args.working_dir))
+        report.add_html_object(SnakePipelineUtils.create_citations_section(['Zankari_2012-resfinder']))
+        report.save()
 
     def __get_db_data(self) -> Dict[str, Any]:
         """
@@ -95,12 +71,15 @@ class MainResFinderLocal(object):
         """
         return {
             'path': self._args.resfinder_db,
-            'min_percent_identity': self._args.min_percent_identity,
-            'min_coverage': self._args.min_percent_coverage,
+            'params': {
+                'blastn': {
+                    'min_percent_identity': self._args.min_percent_identity,
+                    'min_coverage': self._args.min_percent_coverage}},
             'metadata': {'name': 'Antibiotic(s)', 'key': 'antibiotics'}
         }
 
-    def __run_gene_detection(self, fasta_file: ToolIOFile, db_data: Dict[str, Any]) -> None:
+    def __run_gene_detection(self, fasta_file: Path, db_data: Dict[str, Any], report: HtmlReport) -> \
+            GeneDetectionOutput:
         """
         Runs the gene detection workflow.
         :param fasta_file: FASTA file
@@ -108,10 +87,11 @@ class MainResFinderLocal(object):
         :return: None
         """
         wrapper = GeneDetectionWrapper(os.path.join(self._args.working_dir, 'resfinder'))
-        wrapper.run_workflow_blast(fasta_file.path, self._sample_name, db_data)
-        self._report.add_html_object(wrapper.output.report_section)
-        wrapper.output.report_section.copy_files(self._report.output_dir)
-        self._report.save()
+        wrapper.run_workflow_blast(fasta_file, self._sample_name, db_data)
+        report.add_html_object(wrapper.output.report_section)
+        wrapper.output.report_section.copy_files(report.output_dir)
+        report.save()
+        return wrapper.output
 
 
 if __name__ == '__main__':
