7daa7bf7f0077a2a465828e23bbb25eeb6af8441
chdevogelaere <charlotte.devogelaere@sciensano.be>
2021-07-19 15:50:42 +0200

    [GATK4] Add GATK4VAlidateVariants class

---
+++ camel/app/tools/gatk4/gatk4validatevariants.py
@@ -0,0 +1,79 @@
+import shutil
+import logging
+
+from pathlib import Path
+
+from camel.app.camel import Camel
+from camel.app.tools.gatk4.gatk4 import GATK4
+from camel.app.io.tooliofile import ToolIOFile
+
+class GATK4ValidateVariants(GATK4):
+    """
+    Class for GATK4 ValidateVariants function, validate the adherence of a file to VCF format.
+
+    Required inputs:
+    ----------------
+    'VCF':              ToolIOFile object. Input VCF file.
+    'FASTA_REF':        ToolIOFile object. FASTA file containing the reference genome.
+
+    """
+
+    def __init__(self, camel: Camel):
+        """
+        Initialize the GATK4 ValidateVariants tool
+        :param camel: Camel instance
+        :return: None
+        """
+        super().__init__('gatk4 ValidateVariants', '4.1.9.0', camel)
+
+        self._required_inputs = ["VCF", "FASTA_REF"]
+        self._specific_parameters = ["output"]
+
+    def _set_input(self) -> None:
+        """
+        Set the input specification. Overrules method in parent class
+        :return: None
+        """
+        super(GATK4ValidateVariants, self)._set_input()
+
+        if 'VCF_dbsnp' in self._tool_inputs:
+            self._input_string += f"--dbsnp {self._tool_inputs['VCF_dbsnp'][0].path} "
+
+    def _set_output(self):
+        """
+        Set the output specification. Overrules method in parent class
+        :return: None
+        """
+        self._tool_outputs['TXT_metrics'] = [ToolIOFile(Path(self._folder) / self._parameters['output'].value)]
+
+    def _print_output(self):
+        """
+        This function does not create an output file, but outputs everything to stderr
+        Print output file for snakemake's sake
+        :return: None
+        """
+        with open(self._tool_outputs['TXT_metrics'][0].path, 'w') as output_file:
+            output_file.write(self.stderr)
+
+    def _check_command_output(self) -> None:
+        """
+        Check the result. Error in the VCF file should not automatically cause the pipeline to terminate.
+        :return: None
+        """
+        for line in self.stderr.splitlines():
+            if 'ERROR' in line:
+                logging.warning(f"GATK tool {self._name} failed to run, message: \n{self.stderr}")
+
+    def _execute_tool(self) -> None:
+        """
+        Run a GATK function. Overrules method in parent class
+        :return: None
+        """
+        self._set_input()
+        self._set_output()
+        self._set_specific_parameters()
+        self._build_command()
+        self._execute_command()
+        self._print_output()
+        self._set_informs()
+        shutil.rmtree(self._temp_dir)
