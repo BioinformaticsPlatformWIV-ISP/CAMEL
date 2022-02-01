9355894b56a6c789575fc91dca50b5bdd57b2eb5
Bert Bogaerts <bert.bogaerts@sciensano.be>
2022-02-01 13:11:42 +0100

    Implemeted AMRFinder and reporter class

---
+++ camel/app/tools/amrfinder/amrfinder.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from camel.app.camel import Camel
+from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
+from camel.app.error.toolexecutionerror import ToolExecutionError
+from camel.app.io.tooliofile import ToolIOFile
+from camel.app.tools.tool import Tool
+
+
+class AMRFinder(Tool):
+    """
+    NCBI Antimicrobial Resistance Gene Finder (AMRFinderPlus).
+    """
+
+    def __init__(self, camel: Camel) -> None:
+        """
+        Initializes this tool.
+        :param camel: CAMEL instance
+        """
+        super().__init__('AMRFinder', '3.10.18', camel)
+
+    def _check_input(self) -> None:
+        """
+        Checks if the provided input is valid.
+        :return: None
+        """
+        if 'FASTA' not in self._tool_inputs:
+            raise InvalidInputSpecificationError("FASTA input is required")
+        if 'DIR' not in self._tool_inputs:
+            raise InvalidInputSpecificationError("Database input is required (DIR)")
+        super()._check_input()
+
+    def _execute_tool(self) -> None:
+        """
+        Executes this tool.
+        :return: None
+        """
+        output_path = Path(self.folder) / Path(self._parameters['output_path'].value)
+        self._command.command = ' '.join([
+            self._tool_command,
+            '--nucleotide', str(self._tool_inputs['FASTA'][0].path),
+            '--database', str(self._tool_inputs['DIR'][0].path),
+            f"{self._parameters['output_path'].option} {output_path}"
+        ] + self._build_options(['output_path']))
+        self._execute_command()
+        self._tool_outputs['TSV'] = [ToolIOFile(output_path)]
+        self._informs['db_version'] = self._tool_inputs['DIR'][0].path.resolve().name
+
+    def _check_command_output(self) -> None:
+        """
+        Checks if the command executed successfully.
+        :return: None
+        """
+        if not self._command.returncode == 0:
+            raise ToolExecutionError(f"Error executing {self.name}: {self.stderr}")
