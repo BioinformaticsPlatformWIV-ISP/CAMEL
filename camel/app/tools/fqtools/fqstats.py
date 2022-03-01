223836b1cc13d0fad9da3a95e7764fb71f6e65a0
Bert Bogaerts <bert.bogaerts@sciensano.be>
2022-03-01 16:08:51 +0100

    Added fqtools stats and tests

--- camel/app/tools/fqtools/fqstats.py
+++ camel/app/tools/fqtools/fqstats.py
@@ -1,55 +1,63 @@
 import re
+from pathlib import Path
 
+from camel.app.camel import Camel
+from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
 from camel.app.error.toolexecutionerror import ToolExecutionError
 from camel.app.tools.tool import Tool
 
 
-class Fqstats(Tool):
+class FqStats(Tool):
     """
-    Reports the number of sequences & the number of bases from FASTQ files.
+    Reports the number of sequences & the number of bases from input FASTQ files.
     """
 
-    def __init__(self, camel):
+    def __init__(self, camel: Camel) -> None:
         """
         Initializes this tool.
         :param camel: Camel instance
         """
         super().__init__('fqstats', '1.1',  camel)
 
-    def _check_input(self):
+    def _check_input(self) -> None:
         """
-        Checks the tool input.
+        Check if the provided tool input is valid.
         :return: None
         """
         if 'FASTQ' not in self._tool_inputs:
-            raise ValueError("No FASTQ input found.")
-        super(Fqstats, self)._check_input()
+            raise InvalidInputSpecificationError("FASTQ input is required")
+        super()._check_input()
 
-    def _execute_tool(self):
+    def _execute_tool(self) -> None:
         """
         Executes this tool.
         :return: None
         """
+        self._informs['stats'] = []
         for fastq_file in self._tool_inputs['FASTQ']:
-            self.__build_command(fastq_file)
+            self.__build_command(fastq_file.path)
             self._execute_command()
-            self.__add_informs(fastq_file)
+            self.__add_informs(fastq_file. path)
 
-    def __build_command(self, input_file):
+    def __build_command(self, fq_in: Path) -> None:
         """
         Builds the command
-        :param input_file: Input FASTQ file
+        :param fq_in: Input FASTQ file
         :return: None
         """
-        self._command.command = '{} {}'.format(self._tool_command, input_file.path)
+        self._command.command = ' '.join([self._tool_command, str(fq_in)])
 
-    def __add_informs(self, input_file):
+    def __add_informs(self, fq_in: Path) -> None:
         """
         Adds the informs.
-        :param input_file: Input file
+        :param fq_in: Input FASTQ file
         :return: None
         """
-        m = re.match('Found (\d+) sequences, (\d+) bases', self.stdout.strip())
+        m = re.match(r'Found (\d+) sequences, (\d+) bases', self.stdout.strip())
         if not m:
-            raise ToolExecutionError("Error retrieving fqstats from {}.".format(input_file.basename))
-        self._informs[input_file.path] = {'nb_of_sequences': int(m.group(1)), 'nb_of_bases': int(m.group(2))}
+            raise ToolExecutionError(f"Error parsing fqstats for file: {fq_in.name}.")
+        self._informs['stats'].append({
+            'path': str(fq_in),
+            'nb_of_sequences': int(m.group(1)),
+            'nb_of_bases': int(m.group(2))
+        })
