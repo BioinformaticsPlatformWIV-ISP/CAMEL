fb61e8fe3975c6ad8141924a67391197e3e4a1c1
MaGo3828 <maxime.godfroid@sciensano.be>
2023-02-01 11:25:17 +0100

    [MG_seqkit_seq] fixed upsource comments

--- camel/tests/seqkit/test_seqkitseq.py
+++ camel/tests/seqkit/test_seqkitseq.py
@@ -14,25 +14,25 @@ class TestSeqkitSeq(CamelTestSuite):
     FILE_FASTA = ToolIOFile(test_file_dir / 'assembly-VAR305.fasta')
     FILE_FASTQ = ToolIOFile(test_file_dir / 'reads_illumina_1.fastq')
 
-    def test_seqkitseq_fasta(self) -> None:
+    def test_seqkit_seq_fasta(self) -> None:
         """
         Testing SeqKit seq with contigs file.
         """
-        seqkitseq = SeqkitSeq(self.camel)
-        seqkitseq.add_input_files({'FASTA': [TestSeqkitSeq.FILE_FASTA]})
-        seqkitseq.update_parameters(output_filename='sequences.fasta', max_length=100000)
-        seqkitseq.run(self.running_dir)
-        self.verify_output_files(seqkitseq, 'FASTA')
+        seqkit_seq = SeqkitSeq(self.camel)
+        seqkit_seq.add_input_files({'FASTA': [TestSeqkitSeq.FILE_FASTA]})
+        seqkit_seq.update_parameters(output_filename='sequences.fasta', max_length=100000)
+        seqkit_seq.run(self.running_dir)
+        self.verify_output_files(seqkit_seq, 'FASTA')
 
-    def test_seqkitseq_fastq(self) -> None:
+    def test_seqkit_seq_fastq(self) -> None:
         """
         Testing SeqKit seq with paired-end fastq reads.
         """
-        seqkitseq = SeqkitSeq(self.camel)
-        seqkitseq.add_input_files({'FASTQ': [TestSeqkitSeq.FILE_FASTQ]})
-        seqkitseq.update_parameters(max_length=150)
-        seqkitseq.run(self.running_dir)
-        self.verify_output_files(seqkitseq, 'FASTQ')
+        seqkit_seq = SeqkitSeq(self.camel)
+        seqkit_seq.add_input_files({'FASTQ': [TestSeqkitSeq.FILE_FASTQ]})
+        seqkit_seq.update_parameters(max_length=150)
+        seqkit_seq.run(self.running_dir)
+        self.verify_output_files(seqkit_seq, 'FASTQ')
 
 
 if __name__ == '__main__':
