46c4ff5e479ce7bbb69f1c215a6cf730b5eae306
Nathalie Goeders <nathalie.goeders@sciensano.be>
2023-03-30 18:32:26 +0200

    Added a dummy command

--- camel/app/tools/pipelines/neisseria/gmats.py
+++ camel/app/tools/pipelines/neisseria/gmats.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 
 from camel.app.camel import Camel
+from camel.app.io.tooliofile import ToolIOFile
 from camel.app.tools.tool import Tool
 
 
@@ -17,19 +18,24 @@ class GmatsAlgorithm(Tool):
     def _execute_tool(self) -> None:
         """
         Executes this tool.
-        :return: None
+        :return: concatenated tsv files
         """
-        print(f'{self.name} is running!')
+        self._command.command = ' '.join([
+            self._tool_command,
+            ' '.join(str(io.path) for io in self._tool_inputs['TSV']),
+            '> /scratch/nagoeders/PyCharmProjects/RD_NG/camel_3.0/output_file.txt'
+        ])
+        self._execute_command()
 
 
 if __name__ == '__main__':
     gmats = GmatsAlgorithm(Camel.get_instance())
     logging.info('test')
-    gmats.add_input_files({'TSV' : [
-        ToolIOFile(Path('typing-bast-peptide-RRS16BD04259.tsv')),
-        ToolIOFile(Path('typing-bast-peptide-S17BD02954.tsv')),
-        ToolIOFile(Path('typing-bast-peptide-S17BD08805.tsv')),
-        ToolIOFile(Path('typing-bast-peptide-S18BD04144.tsv')),
-        ToolIOFile(Path('typing-bast-peptide-S18BD07986.tsv'))]
+    gmats.add_input_files({'TSV': [
+        ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-RRS16BD04259.tsv')),
+        ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-S17BD02954.tsv')),
+        ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-S17BD08805.tsv')),
+        ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-S18BD04144.tsv')),
+        ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-S18BD07986.tsv'))]
     })
     gmats.run()
