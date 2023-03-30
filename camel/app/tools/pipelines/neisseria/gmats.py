bab244ac1d83bd99910ef80de982e936dea4cfd3
Nathalie Goeders <nathalie.goeders@sciensano.be>
2023-03-30 18:08:02 +0200

    Added path to input files

--- camel/app/tools/pipelines/neisseria/gmats.py
+++ camel/app/tools/pipelines/neisseria/gmats.py
@@ -1,7 +1,12 @@
+import logging
+from pathlib import Path
+
 from camel.app.camel import Camel
 from camel.app.tools.tool import Tool
 
+
 class GmatsAlgorithm(Tool):
+
     def __init__(self, camel: Camel) -> None:
         """
         Initializes this tool.
@@ -15,3 +20,16 @@ class GmatsAlgorithm(Tool):
         :return: None
         """
         print(f'{self.name} is running!')
+
+
+if __name__ == '__main__':
+    gmats = GmatsAlgorithm(Camel.get_instance())
+    logging.info('test')
+    gmats.add_input_files({'TSV' : [
+        ToolIOFile(Path('typing-bast-peptide-RRS16BD04259.tsv')),
+        ToolIOFile(Path('typing-bast-peptide-S17BD02954.tsv')),
+        ToolIOFile(Path('typing-bast-peptide-S17BD08805.tsv')),
+        ToolIOFile(Path('typing-bast-peptide-S18BD04144.tsv')),
+        ToolIOFile(Path('typing-bast-peptide-S18BD07986.tsv'))]
+    })
+    gmats.run()
