f78d72f5b9ddcb2220451c448c03cca0ff4c22f7
MaGo3828 <maxime.godfroid@sciensano.be>
2023-04-06 16:35:58 +0200

    [mg_polca] added polca 4.1.0

---
+++ camel/tests/polca/test_polca.py
@@ -0,0 +1,28 @@
+from camel.app.components.testing.cameltestsuite import CamelTestSuite
+from camel.app.io.tooliofile import ToolIOFile
+from camel.app.tools.polca.polca import Polca
+
+
+class TestPolca(CamelTestSuite):
+    """
+    Tests the Polca tool.
+    """
+    # Get test file and reference file directories
+    test_file_dir = CamelTestSuite.get_test_file_dir('polca')
+
+    # Create ToolIOFile input files
+    FILE_FASTQ_1 = ToolIOFile(test_file_dir / 'reads_trimmed_1.fastq.gz')
+    FILE_FASTQ_2 = ToolIOFile(test_file_dir / 'reads_trimmed_2.fastq.gz')
+    FILE_FASTA_REF = ToolIOFile(test_file_dir / 'consensus.fasta')
+
+    def test_polca(self) -> None:
+        """
+        Tests polca
+        :return: None
+        """
+        polca = Polca(self.camel)
+        polca.add_input_files({
+            'FASTQ_PE': [TestPolca.FILE_FASTQ_1, TestPolca.FILE_FASTQ_2],
+            'FASTA': [TestPolca.FILE_FASTA_REF]})
+        polca.run(self.running_dir)
+        self.verify_output_files(polca, 'FASTA')
