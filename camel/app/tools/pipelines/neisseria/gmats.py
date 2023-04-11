84d4385fcbfc267e83e9e305f6cc166174706463
nagoeders <VSeB!2022>
2023-04-11 16:00:23 +0200

    Added implementation of gMATS status at both the allele and sample level

--- camel/app/tools/pipelines/neisseria/gmats.py
+++ camel/app/tools/pipelines/neisseria/gmats.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pandas as pd
 
 from pathlib import Path
 
@@ -12,30 +13,34 @@ class GmatsAlgorithm(Tool):
 
     def __init__(self, camel: Camel) -> None:
         """
-        Initializes this tool.
+        Initializes GmatsAlgorithm.
         :param camel: CAMEL instance
         """
         super().__init__('gMATS Algorithm', '0.1', camel)
 
     def _execute_tool(self) -> None:
         """
-        Executes this tool.
-        :return: concatenated tsv files
+        Executes GmatsAlgorithm.
+        :return: text file with gMATS profile
         """
 
         output_dir = self._parameters['output_directory'].value
+        gmats_db = pd.read_csv('/var/lib/.bioit_database/pipelines/neisseria/gMATS_DB.txt', sep='\t')
+        fhbp_db = gmats_db.loc[gmats_db['Locus'] == 'fHbp']
+        nhba_db = gmats_db.loc[gmats_db['Locus'] == 'NHBA']
 
         for key in self._tool_inputs['TSV']:
-            print(key)
+            print('Input file : ', key)
             f = open(key.path, "r")
 
-            sample_ID = os.path.basename(key.path).replace('.tsv', '').replace('typing-bast-peptide-', '')
-            print('Sample_ID : ', sample_ID)
+            sample_id = os.path.basename(key.path).replace('.tsv', '').replace('typing-bast-peptide-', '')
+            print('sample_ID : ', sample_id)
 
-            output_file = Path(output_dir + '/' + sample_ID + '_gMATS.tsv')
+            output_file = Path(output_dir + '/' + sample_id + '_gMATS.tsv')
             print('Output file : ', output_file)
 
-            with open(output_file, mode = 'w') as o:
+            # Write output backbone
+            with open(output_file, mode='w') as o:
                 o.write('Locus' + '\t' +
                         'Allele' + '\t' +
                         '% Identity' + '\t' +
@@ -44,13 +49,14 @@ class GmatsAlgorithm(Tool):
                         'gMATS status' + '\n')
                 f.close()
 
+            # Prepare lists to collect output
             locus = ['fHbp', 'NHBA', 'NadA', 'PorA_VR2']
             allele = []
             id_pct = []
             coverage = []
             allele_status = []
-            gmats_status = ['-', '-', '-', '-']
 
+            # Derive output from input file and gMATS database
             with open(key.path) as f:
                 for line in f:
                     l = line.split('\t')
@@ -59,36 +65,54 @@ class GmatsAlgorithm(Tool):
                         allele.append(l[1])
                         id_pct.append(l[2])
                         coverage.append(l[3])
-                        allele_status.append('')
+                        if l[1] in ['-', '?']:
+                            allele_status.append('unpredictable')
+                        elif float(l[1]) in fhbp_db.Allele:
+                            allele_status.append(fhbp_db.Status.loc[fhbp_db['Allele'] == float(l[1])].iloc[0])
+                        else:
+                            allele_status.append('unpredictable')
                     elif l[0] == 'NHBA_peptide':
                         allele.append(l[1])
                         id_pct.append(l[2])
                         coverage.append(l[3])
-                        allele_status.append('')
+                        if l[1] in ['-', '?']:
+                            allele_status.append('unpredictable')
+                        elif float(l[1]) in nhba_db.Allele:
+                            print(float(l[1]))
+                            print(nhba_db.Status.loc[nhba_db['Allele'] == float(l[1])])
+                            allele_status.append(nhba_db.Status.loc[nhba_db['Allele'] == float(l[1])].iloc[0])
+                        else:
+                            allele_status.append('unpredictable')
                     elif l[0] == 'NadA_peptide':
                         allele.append(l[1])
                         id_pct.append(l[2])
                         coverage.append(l[3])
-                        allele_status.append('Not covered')
+                        if l[1] in ['-', '?']:
+                            allele_status.append('unpredictable')
+                        else:
+                            allele_status.append('not_covered')
                     elif l[0] == 'PorA_VR2':
                         allele.append(l[1])
                         id_pct.append(l[2])
                         coverage.append(l[3])
                         if l[1] == '4':
-                            allele_status.append('Covered')
+                            allele_status.append('covered')
+                        elif l[1] in ['-', '?']:
+                            allele_status.append('unpredictable')
                         else:
-                            allele_status.append('Not covered')
+                            allele_status.append('not_covered')
 
-            print('Loci :' + str(locus))
-            print('Alleles :' + str(allele))
-            print('ID_pct :' + str(id_pct))
-            print('Coverage :' + str(coverage))
-            print('Allele status' + str(allele_status))
-            print('gMATS status' + str(gmats_status))
+            # Derive gMATS status of sample
+            if 'covered' in allele_status:
+                gmats_status = ['covered'] * 4
+            elif 'not_covered' in allele_status:
+                gmats_status = ['not_covered'] * 4
+            else:
+                gmats_status = ['unpredictable'] * 4
 
-            with open(output_file, mode = 'a') as o:
+            # Write output text file
+            with open(output_file, mode='a') as o:
                 for i in range(len(locus)):
-                    print(i)
                     o.write(locus[i] + '\t' +
                             allele[i] + '\t' +
                             id_pct[i] + '\t' +
@@ -97,9 +121,9 @@ class GmatsAlgorithm(Tool):
                             gmats_status[i] + '\n')
                     f.close()
 
-#TODO : infer allele status -> best way to proceed?
-#TODO : infer sample gMATS status
 
+#TODO : check peptide integrity via camel
+#TODO : Add flag for 'Not covered' alleles with a high identity or coverage length with 'Covered' alleles
 
 if __name__ == '__main__':
     gmats = GmatsAlgorithm(Camel.get_instance())
@@ -115,3 +139,12 @@ if __name__ == '__main__':
     gmats.run()
     logging.info('test3')
     print(gmats.tool_outputs)
+
+#TODO : automatic list of input files
+
+
+
+
+
+
+
