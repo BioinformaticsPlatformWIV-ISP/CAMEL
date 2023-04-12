ae95a653534795ddd1b4493a30b8f738b80d3276
nagoeders <VSeB!2022>
2023-04-12 17:08:49 +0200

    Updated script based on BB's review

--- camel/app/tools/pipelines/neisseria/gmats.py
+++ camel/app/tools/pipelines/neisseria/gmats.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import pandas as pd
 
 from pathlib import Path
@@ -7,53 +6,65 @@ from pathlib import Path
 from camel.app.camel import Camel
 from camel.app.io.tooliofile import ToolIOFile
 from camel.app.tools.tool import Tool
+from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
 
-class GmatsAlgorithm(Tool):
+
+class GMats(Tool):
+    """
+    gMATS is the genetic Meningococcal Antigen Typing System (gMATS) for Neisseria meningitidis
+    used to predict the Bexsero vaccine effectiveness.
+    """
 
     def __init__(self, camel: Camel) -> None:
         """
-        Initializes GmatsAlgorithm.
+        Initializes the GMats tool.
         :param camel: CAMEL instance
         """
         super().__init__('gMATS Algorithm', '0.1', camel)
 
+    def _check_input(self) -> None:
+        """
+        Checks if the provided input is valid.
+        :return: None
+        """
+        if 'output_directory' not in self._parameters:
+            raise InvalidInputSpecificationError("Output directory is required.")
+        if 'gmats_db' not in self._parameters:
+            raise InvalidInputSpecificationError("gMATS database is required")
+        if 'TSV' not in self._tool_inputs:
+            raise InvalidInputSpecificationError("List of input files (TSV) is required")
+        super()._check_input()
+
     def _execute_tool(self) -> None:
         """
-        Executes GmatsAlgorithm.
-        :return: text file with gMATS profile
+        Executes the tool.
+        :return: 'None'
         """
 
-        output_dir = self._parameters['output_directory'].value
-        gmats_db = pd.read_csv('/var/lib/.bioit_database/pipelines/neisseria/gMATS_DB.txt', sep='\t')
+        output_dir = Path(self._parameters["output_directory"].value)
+        gmats_db = pd.read_csv(self._parameters['gmats_db'].value, sep='\t')
 
-        for key in self._tool_inputs['TSV']:
-            print('Input file : ', key)
-            f = open(key.path, "r")
+        for path_io in self._tool_inputs['TSV']:
+            logging.info(f'Processing input file: {path_io}')
 
-            sample_id = os.path.basename(key.path).replace('.tsv', '').replace('typing-bast-peptide-', '')
-            print('sample_ID : ', sample_id)
+            sample_id = path_io.path.stem.replace('typing-bast-peptide-', '')
+            logging.info(f'Sample ID: {sample_id}')
 
-            output_file = Path(output_dir + '/' + sample_id + '_gMATS.tsv')
-            print('Output file : ', output_file)
+            output_file = output_dir / f'{sample_id}_gMATS.tsv'
+            logging.info(f'Output file: {output_file}')
 
             # Write output backbone
-            with open(output_file, mode='w') as o:
-                o.write('Locus' + '\t' +
-                        'Allele' + '\t' +
-                        '% Identity' + '\t' +
-                        'HSP/Locus length' + '\t' +
-                        'Allele status' + '\t' +
-                        'gMATS status' + '\n')
-                f.close()
+            with open(output_file, mode='w') as handle:
+                handle.write('\t'.join(['Locus', 'Allele', '% Identity', 'HSP/Locus length', 'Allele status', 'gMATS status', '\n']))
 
             # Parse input file
-            antigen_info = self.__parse_input_file(key.path)
+            antigen_info = self.__parse_input_file(path_io.path)
 
             # Derive gMATS status for each antigen
             allele_status = []
-            for key in antigen_info:
-                allele_gmats = self.__derive_allele_gMATS(key, antigen_info[key][1], gmats_db)
-                antigen_info[key] += [str(allele_gmats )]
+            for locus in antigen_info:
+                allele_gmats = self.__derive_allele_gmats(locus, antigen_info[locus][1], gmats_db)
+                antigen_info[locus] += [str(allele_gmats)]
                 allele_status.append(allele_gmats)
 
             # Derive gMATS status of sample
@@ -65,31 +76,37 @@ class GmatsAlgorithm(Tool):
                 gmats_status = 'unpredictable'
 
             # Write output text file
-            with open(output_file, mode='a') as o:
-                for key in antigen_info:
-                    o.write(antigen_info[key][0] + '\t' +
-                            antigen_info[key][1] + '\t' +
-                            antigen_info[key][2] + '\t' +
-                            antigen_info[key][3] + '\t' +
-                            antigen_info[key][4] + '\t' +
-                            gmats_status + '\n')
-                    f.close()
+            with open(output_file, mode='a') as handle:
+                for locus in antigen_info:
+                    handle.write('\t'.join([antigen_info[locus][0],
+                                            str(antigen_info[locus][1]),
+                                            str(antigen_info[locus][2]),
+                                            str(antigen_info[locus][3]),
+                                            antigen_info[locus][4],
+                                            gmats_status,
+                                            '\n']))
+                handle.close()
 
-    def __parse_input_file(self, file_path):
+    def __parse_input_file(self, file_path: Path) -> dict[str, list[str]]:
         """
         Parses the input file.
         :param file_path: Path to input text file
         :return: dictionary with 'Locus', 'Allele', '% identity' for each of the 4 antigens
         """
-        allele_info = {}
-        with file_path.open() as f:
-            for line in f:
-                l = line.split('\t')
-                if l[0] in ['fHbp_peptide', 'NHBA_peptide', 'NadA_peptide', 'PorA_VR2']:
-                    allele_info[l[0]] = l[0:4]
+        input_file = pd.read_table(file_path)
+
+        allele_info = {'fHbp_peptide': input_file.loc[input_file['Locus'] == 'fHbp_peptide'].iloc[0,
+                                       0:4].values.flatten().tolist(),
+                       'NHBA_peptide': input_file.loc[input_file['Locus'] == 'NHBA_peptide'].iloc[0,
+                                       0:4].values.flatten().tolist(),
+                       'NadA_peptide': input_file.loc[input_file['Locus'] == 'NadA_peptide'].iloc[0,
+                                       0:4].values.flatten().tolist(),
+                       'PorA_VR2': input_file.loc[input_file['Locus'] == 'PorA_VR2'].iloc[0,
+                                   0:4].values.flatten().tolist()}
+
         return allele_info
 
-    def __derive_allele_gMATS(self, locus, allele, gmats_db):
+    def __derive_allele_gmats(self, locus: str, allele: str, gmats_db: pd.DataFrame) -> str:
         """
         Derive gMATS prediction for locus/allele.
         :param locus: Locus to be assessed
@@ -122,8 +139,9 @@ class GmatsAlgorithm(Tool):
 #TODO : check peptide integrity via camel
 #TODO : Add flag for 'Not covered' alleles with a high identity or coverage length with 'Covered' alleles
 
+
 if __name__ == '__main__':
-    gmats = GmatsAlgorithm(Camel.get_instance())
+    gmats = GMats(Camel.get_instance())
     gmats.add_input_files({'TSV': [
         ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-RRS16BD04259.tsv')),
         ToolIOFile(Path('/testdata/camel/pipelines/gMATS/typing-bast-peptide-S17BD02954.tsv')),
@@ -135,10 +153,3 @@ if __name__ == '__main__':
     logging.info('Done')
 
 #TODO : automatically list input files
-
-
-
-
-
-
-
