cc23a574c3aac3df7b9ce455f772c72f05b2c09f
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-06-13 14:57:09 +0200

    Bumped seqkit version and implemented seqkit stats

--- camel/app/tools/seqkit/seqkitstats.py
+++ camel/app/tools/seqkit/seqkitstats.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import pandas as pd
 
 from camel.app.camel import Camel
 from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
@@ -7,29 +7,28 @@ from camel.app.io.tooliofile import ToolIOFile
 from camel.app.tools.tool import Tool
 
 
-class SeqkitSeq(Tool):
-
+class SeqkitStats(Tool):
     """
-    Seqkit seq performs common transformations of FASTA / FASTQ files.
+    Seqkit stats reports simple statistics of FASTA/Q files.
     """
 
     INPUT_KEYS = ('FASTQ', 'FASTA')
 
     def __init__(self, camel: Camel) -> None:
         """
-        Initialize seqkit seq.
+        Initializes this tool.
         :param camel: Camel instance
         :return: None
         """
-        super().__init__('Seqkit seq', '2.3.1', camel)
+        super().__init__('Seqkit stats', '2.3.1', camel)
 
     def _check_input(self) -> None:
         """
         Checks if the provided input is valid.
         :return: None
         """
-        if not any(x in self._tool_inputs for x in SeqkitSeq.INPUT_KEYS):
-            raise InvalidInputSpecificationError('{} input is required.'.format(' or '.join(SeqkitSeq.INPUT_KEYS)))
+        if not any(x in self._tool_inputs for x in SeqkitStats.INPUT_KEYS):
+            raise InvalidInputSpecificationError('{} input is required.'.format(' or '.join(SeqkitStats.INPUT_KEYS)))
         super()._check_input()
 
     def _execute_tool(self) -> None:
@@ -37,10 +36,13 @@ class SeqkitSeq(Tool):
         Executes this tool.
         :return: None
         """
-        output_path = self._folder / self._parameters['output_filename'].value
+        tsv_out = self.folder / self._parameters['output_filename'].value
         self.__build_command()
         self._execute_command()
-        self.__set_output(output_path)
+        data = pd.read_table(tsv_out)
+        for k, v in next(iter(data.to_dict('records'))).items():
+            self._informs[k] = v
+        self._tool_outputs['TSV'] = [ToolIOFile(tsv_out)]
 
     def __build_command(self) -> None:
         """
@@ -50,18 +52,10 @@ class SeqkitSeq(Tool):
         input_key = 'FASTQ' if 'FASTQ' in self._tool_inputs else 'FASTA'
         self._command.command = ' '.join([
             self._tool_command,
+            '--all', '--tabular',
             ' '.join([str(f.path) for f in self._tool_inputs[input_key]]),
             *self._build_options()
         ])
-
-    def __set_output(self, output_path: Path) -> None:
-        """
-        Sets the tool output.
-        :param output_path: Output path
-        :return: None
-        """
-        output_key = 'FASTA' if output_path.name.lower().endswith('.fasta') else 'FASTQ'
-        self._tool_outputs[output_key] = [ToolIOFile(output_path)]
 
     def _check_command_output(self) -> None:
         """
