9f56291d7750de8930a8c0d7c94b8384ce143f52
nagoeders <VSeB!2022>
2023-07-11 11:59:52 +0200

    Update to use a single FASTA file as input

--- camel/app/tools/pipelines/neisseria/characterizeneisseriacapsule.py
+++ camel/app/tools/pipelines/neisseria/characterizeneisseriacapsule.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import tempfile
 from pathlib import Path
 
 from camel.app.camel import Camel
@@ -30,22 +33,22 @@ class CharacterizeNeisseriaCapsule(Tool):
             raise InvalidInputSpecificationError('Required input directory containing the fasta files is missing')
         super()._check_input()
 
-    def __build_input_string(self) -> str:
+    def __build_input_string(self, dir_path: Path, dir_out: Path) -> str:
         """
         Creates the string with the input files
         :return: String with the input parameters
         """
-        inputs = [f"-o {self._parameters['output_directory'].value}", f"-t {self._parameters['threads'].value}"]
-        if 'FASTA_dir' in self._tool_inputs:
-            inputs.append(f"-d {self._tool_inputs['FASTA_dir'][0].path}")
+        inputs = [f"-o {dir_out}",
+                  f"-t {self._parameters['threads'].value}",
+                  f"-d {dir_path}"]
         return ' '.join(inputs)
 
-    def __build_command(self) -> None:
+    def __build_command(self, dir_path: Path, dir_out: Path) -> None:
         """
         Concatenates required parameters and options to build the command.
         :return: None
         """
-        input_string = self.__build_input_string()
+        input_string = self.__build_input_string(dir_path, dir_out)
         self._command.command = ' '.join([self._tool_command, input_string])
 
     def _check_command_output(self) -> None:
@@ -61,10 +64,26 @@ class CharacterizeNeisseriaCapsule(Tool):
         Executes this tool.
         :return: None
         """
-        self.__build_command()
-        self._execute_command()
-        dir_out = self.folder / Path(self._parameters['output_directory'].value)
+        temp_input_dir = tempfile.TemporaryDirectory()
         try:
-            self._tool_outputs['TSV'] = [ToolIOFile(next((dir_out / 'serogroup').glob('serogroup_predictions_*.tab')))]
-        except StopIteration:
-            raise ToolExecutionError(f"TSV file not found in output folder: {dir_out / 'serogroup'}")
+            for filename in os.scandir(self._tool_inputs['FASTA_dir'][0].path):
+                if os.path.splitext(filename)[-1] == '.fasta':
+                    # Temporary copy to use a single FASTA file as input
+                    shutil.copy(filename.path, Path(temp_input_dir.name))
+
+                    # Execute the tool
+                    sample_name = os.path.splitext(os.path.split(filename)[-1])[0]
+                    dir_out = Path(f"{self._parameters['output_directory'].value}/{sample_name}")
+                    self.__build_command(temp_input_dir.name, dir_out)
+                    self._execute_command()
+
+                    # Remove temporary fasta file
+                    filepath = os.path.join(temp_input_dir.name, os.path.basename(filename))
+                    os.remove(filepath)
+                try:
+                    self._tool_outputs['TSV'] = [ToolIOFile(next((dir_out / 'serogroup').glob('serogroup_predictions_'
+                                                                                              '*.tab')))]
+                except StopIteration:
+                    raise ToolExecutionError(f"TSV file not found in output folder: {dir_out / 'serogroup'}")
+        finally:
+            temp_input_dir.cleanup()
