a2ce2a4226125e46be350b1c412a048461d9579b
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-07-12 15:06:47 +0200

    [Novel assay] Added the novel serogroup determination tool to the Neisseria pipeline

--- camel/scripts/neisseriapipeline/snakefile/serogroup_determination.py
+++ camel/scripts/neisseriapipeline/snakefile/serogroup_determination.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 SNAKEFILE_SEROGROUP_DETERMINATION = f'{Path(__file__).parent / Path(__file__).stem}.smk'
 _dir_serogroup = Path('serogroup_determination')
-OUTPUT_SEROGROUP_DETERMINATION_REPORT = _dir_serogroup / 'html.io'
-OUTPUT_SEROGROUP_DETERMINATION_REPORT_EMPTY = _dir_serogroup / 'html-empty.io'
+OUTPUT_SEROGROUP_DETERMINATION_LEGACY_REPORT = _dir_serogroup / 'legacy' / 'html.io'
+OUTPUT_SEROGROUP_DETERMINATION_LEGACY_REPORT_EMPTY = _dir_serogroup / 'legacy' / 'html-empty.io'
+OUTPUT_SEROGROUP_DETERMINATION_REPORT = _dir_serogroup / 'capsule' / 'html.io'
+OUTPUT_SEROGROUP_DETERMINATION_INFORMS = _dir_serogroup / 'capsule' / 'informs.io'
+OUTPUT_SEROGROUP_DETERMINATION_REPORT_EMPTY = _dir_serogroup / 'capsule' / 'html-empty.io'
 OUTPUT_SEROGROUP_DETERMINATION_SUMMARY = _dir_serogroup / 'summary_out.tsv'
