239bf4ab83d40d01948afd89267c428da14f2b92
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-12-06 16:54:44 +0100

    Updated quality checks for compatibility with multiple input types

--- camel/resources/snakefile/quality_checks.py
+++ camel/resources/snakefile/quality_checks.py
@@ -1,104 +1,62 @@
+import dataclasses
 import operator
-from dataclasses import dataclass, asdict
+from dataclasses import asdict
 from pathlib import Path
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 
-from camel.resources.snakefile import medaka_polishing
-from camel.resources.snakefile.assembly_spades import OUTPUT_ASSEMBLY_MAPPING_INFORMS, OUTPUT_ASSEMBLY_DEPTH_INFORMS
-from camel.resources.snakefile.variant_calling import OUTPUT_VARIANT_CALLING_MAPPING_INFORMS, \
-    OUTPUT_VARIANT_CALLING_DEPTH_INFORMS
+from camel.resources.snakefile import assembly_spades, assembly_flye
+
 
 SNAKEFILE_QUALITY_CHECKS = f'{Path(__file__).parent / Path(__file__).stem}.smk'
-_dir_qc = Path('quality_checks', '{read_type}')
+_dir_qc = Path('quality_checks')
 OUTPUT_QUALITY_CHECKS_REPORT = _dir_qc / 'report' / 'html.io'
 OUTPUT_QUALITY_CHECKS_SUMMARY = _dir_qc / 'summary' / 'summary_out.tsv'
 OUTPUT_QUALITY_CHECKS_REPORT_JSON = _dir_qc / 'report' / 'informs.json'
 
 
-# def get_mapping_rate_informs(config: Dict, read_type: str = None) -> Path:
-def get_mapping_rate_informs(config: Dict, read_type: str = None) -> Path:
+def get_mapping_rate_informs(config: Dict, read_key: str) -> Path:
     """
     Returns the input for the mapping rate.
-    :param read_type: read type
     :param config: Snakemake config
+    :param read_key: Read key ('fastq_se' or 'fastq_pe')
     :return: Mapping rate informs
     """
-    if read_type is None:
-        read_type = config['read_type']
-    assembly_type_variable = config.get('wildcards_assembly', 'long_read_assembly')
     mode = config['quality_checks'].get('coverage_mode', 'assembly')
-    if read_type == 'nanopore':
-        # return Path(config['working_dir']) / assembly_canu.OUTPUT_ASSEMBLY_MAPPING_RATE_INFORMS
-        return Path(config['working_dir']) / \
-               str(medaka_polishing.OUTPUT_ASSEMBLY_MAPPING_RATE_INFORMS).format(assembly_type=assembly_type_variable)
-        # return Path(config['working_dir']) / assembly_flye.OUTPUT_ASSEMBLY_MAPPING_RATE_INFORMS
-    elif mode == 'assembly':
-        return Path(config['working_dir']) / OUTPUT_ASSEMBLY_MAPPING_INFORMS
-    elif mode == 'ref':
-        return Path(config['working_dir']) / OUTPUT_VARIANT_CALLING_MAPPING_INFORMS
-    raise ValueError(f"Invalid coverage mode: '{mode}'")
+    if mode == 'assembly':
+        if read_key == 'fastq_pe':
+            return Path(config['working_dir'], assembly_spades.OUTPUT_ASSEMBLY_MAPPING_INFORMS)
+        elif read_key == 'fastq_se':
+            return Path(config['working_dir'], assembly_flye.OUTPUT_ASSEMBLY_MAPPING_RATE_INFORMS)
+    raise ValueError(f'Cannot determine mapping informs')
 
 
-def get_depth_informs(config: Dict, read_type: str = None) -> Path:
+def get_depth_informs(config: Dict, read_key: str = None) -> Path:
     """
     Returns the input for the depth.
-    :param read_type: read type
     :param config: Snakemake config
+    :param read_key: Read key ('fastq_se' or 'fastq_pe')
     :return: Depth informs
     """
-    if read_type is None:
-        read_type = config['read_type']
-    assembly_type_variable = config.get('wildcards_assembly', 'long_read_assembly')
     mode = config['quality_checks'].get('coverage_mode', 'assembly')
-    if read_type == 'nanopore':
-        # return Path(config['working_dir']) / assembly_canu.OUTPUT_ASSEMBLY_DEPTH_INFORMS
-        # return Path(config['working_dir']) / assembly_flye.OUTPUT_ASSEMBLY_DEPTH_INFORMS
-        return Path(config['working_dir']) / \
-               str(medaka_polishing.OUTPUT_ASSEMBLY_DEPTH_INFORMS).format(assembly_type=assembly_type_variable)
-    elif mode == 'assembly':
-        return Path(config['working_dir']) / OUTPUT_ASSEMBLY_DEPTH_INFORMS
-    elif mode == 'ref':
-        return Path(config['working_dir']) / OUTPUT_VARIANT_CALLING_DEPTH_INFORMS
-    raise ValueError(f"Invalid coverage mode: '{mode}'")
+    if mode == 'assembly':
+        if read_key == 'fastq_pe':
+            return Path(config['working_dir'], assembly_spades.OUTPUT_ASSEMBLY_DEPTH_INFORMS)
+        elif read_key == 'fastq_se':
+            return Path(config['working_dir'], assembly_flye.OUTPUT_ASSEMBLY_DEPTH_INFORMS)
+    raise ValueError(f"Cannot determine depth informs: '{mode}'")
 
 
-def get_qc_report(config: Dict, read_type: str = None) -> Path:
-    """
-    Returns the QC report.
-    :param config: configuration dictionary
-    :param read_type: read type
-    :return: Path to the report
-    """
-    if read_type is None:
-        read_type = config['read_type']
-    if read_type not in ['illumina', 'nanopore']:
-        raise ValueError(f'Invalid read type: {read_type}')
-    return Path(config['working_dir']) / str(OUTPUT_QUALITY_CHECKS_REPORT).format(read_type=read_type)
-
-
-def get_qc_summary(config: Dict, read_type: str = None) -> Path:
-    """
-    Returns the QC report.
-    :param config: configuration dictionary
-    :param read_type: read type
-    :return: Path to the report
-    """
-    if read_type is None:
-        read_type = config['read_type']
-    if read_type not in ['illumina', 'nanopore']:
-        raise ValueError(f'Invalid read type: {read_type}')
-    return Path(config['working_dir']) / str(OUTPUT_QUALITY_CHECKS_SUMMARY).format(read_type=read_type)
-
-
-@dataclass
+@dataclasses.dataclass
 class QCCheck:
     key: str
     full_name: str
     threshold_warn: float
     threshold_fail: float
+    supported_input_types: dataclasses.field(default_factory=list)
     fmt_string_value: Optional[str]
     value_should_exceed: Optional[bool] = True
     explanation: Optional[str] = None
+    is_default: bool = True
 
     def to_dict(self, value: Optional[float] = None) -> Dict[Any, str]:
         """
@@ -121,42 +79,199 @@ class QCCheck:
 
 
 QC_CHECKS_BY_KEY = {qc.key: qc for qc in [
-    # General
-    QCCheck('cgmlst', 'Typing loci detected (%)', 95.0, 90.0, '{:.2f}%'),
-    QCCheck('kraken', 'Kraken: contaminants', 1.0, 5.0, '{:.2f}%', False),
-    QCCheck('map_rate_ref', 'Reads mapping to reference genome', 95.0, 90.0, '{:.2f}%'),
-    QCCheck('cov_ref', 'Coverage against reference genome', 20, 10, '{:.2f}x'),
-    QCCheck('map_rate_assembly', 'Reads mapping to the assembled contigs', 95.0, 90.0, '{:.2f}%'),
-    QCCheck('cov_assembly', 'Coverage against the assembled contigs', 20, 10, '{:.2f}x'),
-    QCCheck('assembly_total_len', 'Total assembly length deviation', 10, 20, '{:.2f}%', value_should_exceed=False,
-            explanation='Percent deviation from the expected genome size.'),
-    QCCheck('confindr', 'ConFindr: number of contaminating SNPs', 10, 20, '{:,}', value_should_exceed=False),
-    QCCheck('busco', 'Percentage of complete BUSCO genes', 90.0, 80.0, '{:.2f}%',
-            explanation='BUSCO Benchmarking Universal Single-Copy Orthologs) can be used to estimate the completeness '
-                        'of an assembly.'),
-    # Illumina
-    QCCheck('fqc_gc', 'FastQC: GC-content deviation', 2.0, 4.0, '{:.2f}%', False,
-            explanation='checks if the detected GC content is close enough to the expected GC content for this organism'
-                        ' (<b>{:.2f}%</b>).'),
-    QCCheck('fqc_avg_qual', 'FastQC: Average quality score', 30.0, 25.0, '{:.0f}',
-            explanation='checks if the average read quality is above the given threshold.'),
-    QCCheck('fqc_n_fraction', 'FastQC: Max. N-fraction', 0.005, 0.010, '{:.4f}', False,
-            explanation='checks if the maximal N fraction at any read position is below the given threshold.'),
-    QCCheck('fqc_per_base', 'FastQC: Per-base sequence content', 3.0, 6.0, '{:.2f}%', False,
-            explanation='checks if the difference between A-T and C-G is below the given threshold at every position. '
-                        'The first 20 and last 5 bases of the reads are skipped, as the peaks there can be caused by '
-                        'the library kit or trimming artifacts.'),
-    QCCheck('fqc_qscore', 'FastQC: Q-score drop', 200, 150, '{:.0f}',
-            explanation='checks whether the average position in the reads where the mean Q-score drops below <b>30</b> '
-                        'is above the given threshold.'),
-    QCCheck('fqc_seq_len', 'FastQC: Sequence length distribution', 66.67, 40.00, '{:.2f}%',
-            explanation='checks if the median read length of the trimmed reads is below a threshold compared to the '
-                        'mode length of the raw input reads (<b>{}</b>).'),
-
-    # Nanopore
-    QCCheck('nanoplot_len', 'NanoPlot: Median read length', 500, 250, '{:,}'),
-    QCCheck('nanoplot_qual', 'NanoPlot: Median read quality', 10, 8, '{:.2f}'),
-    QCCheck('seqkit_gc', 'seqkit: GC-content deviation', 2.0, 4.0, '{:.2f}%', False,
-            explanation='checks if the detected GC content is close enough to the expected GC content for this organism'
-                        ' (<b>{:.2f}%</b>).')
+    QCCheck(
+        key='kraken_illumina',
+        full_name='Kraken: contaminants (Illumina)',
+        threshold_warn=1.0,
+        threshold_fail=5.0,
+        fmt_string_value='{:.2f}%',
+        supported_input_types=['hybrid', 'illumina'],
+        value_should_exceed=False),
+    QCCheck(
+        key='kraken_ont',
+        full_name='Kraken: contaminants (ONT)',
+        threshold_warn=1.0,
+        threshold_fail=5.0,
+        fmt_string_value='{:.2f}%',
+        supported_input_types=['hybrid', 'ont'],
+        value_should_exceed=False),
+    QCCheck(
+        key='confindr',
+        full_name='ConFindr: number of contaminating SNPs',
+        threshold_warn=10,
+        threshold_fail=20,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:,}',
+        value_should_exceed=False),
+    QCCheck(
+        key='typing_loci',
+        full_name='Typing loci detected (%)',
+        threshold_warn=95.0,
+        threshold_fail=90.0,
+        fmt_string_value='{:.2f}%',
+        supported_input_types=['hybrid', 'illumina', 'ont']),
+    QCCheck(
+        key='map_rate_ref_illumina',
+        full_name='Reads mapping to reference genome (Illumina)',
+        threshold_warn=95.0,
+        threshold_fail=90.0,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}%',
+        is_default=False),
+    QCCheck(
+        key='map_rate_ref_ont',
+        full_name='Reads mapping to reference genome (ONT)',
+        threshold_warn=95.0,
+        threshold_fail=90.0,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:.2f}%',
+        is_default=False),
+    QCCheck(
+        key='map_rate_assembly_illumina',
+        full_name='Reads mapping to the assembled contigs (Illumina)',
+        threshold_warn=95.0,
+        threshold_fail=90.0,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}%'),
+    QCCheck(
+        key='map_rate_assembly_ont',
+        full_name='Reads mapping to the assembled contigs (ONT)',
+        threshold_warn=95.0,
+        threshold_fail=90.0,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:.2f}%'),
+    QCCheck(
+        key='cov_assembly_illumina',
+        full_name='Coverage against the assembled contigs (Illumina)',
+        threshold_warn=20,
+        threshold_fail=10,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}x'),
+    QCCheck(
+        key='cov_assembly_ont',
+        full_name='Coverage against the assembled contigs (ONT)',
+        threshold_warn=20,
+        threshold_fail=10,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:.2f}x'),
+    QCCheck(
+        key='assembly_total_len',
+        full_name='Total assembly length deviation',
+        threshold_warn=10,
+        threshold_fail=20,
+        supported_input_types=['fasta', 'hybrid', 'illumina', 'ont'],
+        fmt_string_value='{:.2f}%',
+        value_should_exceed=False,
+        explanation='Percent deviation from the expected genome size.'),
+    QCCheck(
+        key='busco',
+        full_name='Percentage of complete BUSCO genes',
+        threshold_warn=90.0,
+        threshold_fail=80.0,
+        fmt_string_value='{:.2f}%',
+        supported_input_types=['fasta', 'hybrid', 'illumina', 'ont'],
+        explanation='BUSCO Benchmarking Universal Single-Copy Orthologs) can be used to estimate the completeness of '
+                    'an assembly.'),
+    QCCheck(
+        key='fqc_avg_qual_{ori}',
+        full_name='FastQC: Average quality score',
+        threshold_warn=30.0,
+        threshold_fail=25.0,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.0f}',
+        explanation='checks if the average read quality is above the given threshold.'),
+    QCCheck(
+        key='fqc_gc_{ori}',
+        full_name='FastQC: GC-content deviation',
+        threshold_warn=2.0,
+        threshold_fail=4.0,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}%',
+        value_should_exceed=False,
+        explanation='checks if the detected %GC-content is close enough to the expected value for this organism '
+                    '(<b>{:.2f}%</b>).'),
+    QCCheck(
+        key='fqc_n_fraction_{ori}',
+        full_name='FastQC: Max. N-fraction',
+        threshold_warn=0.005,
+        threshold_fail=0.010,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.4f}',
+        value_should_exceed=False,
+        explanation='checks if the maximal N fraction at any read position is below the given threshold.'),
+    QCCheck(
+        key='fqc_per_base_{ori}',
+        full_name='FastQC: Per-base sequence content',
+        threshold_warn=3.0,
+        threshold_fail=6.0,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}%',
+        value_should_exceed=False,
+        explanation='checks if the difference between A-T and C-G is below the given threshold at every position. The '
+                    'first 20 and last 5 bases of the reads are skipped, as the peaks there can be caused by the '
+                    'library kit or trimming artifacts.'),
+    QCCheck(
+        key='fqc_qscore_{ori}',
+        full_name='FastQC: Q-score drop',
+        threshold_warn=200,
+        threshold_fail=150,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.0f}',
+        explanation='checks whether the average position in the reads where the mean Q-score drops below <b>30</b> is '
+                    'above the given threshold.'),
+    QCCheck(
+        key='fqc_seq_len_{ori}',
+        full_name='FastQC: Sequence length distribution',
+        threshold_warn=66.67,
+        threshold_fail=40.00,
+        supported_input_types=['hybrid', 'illumina'],
+        fmt_string_value='{:.2f}%',
+        explanation='checks if the median read length of the trimmed reads is below a threshold compared to the mode '
+                    'length of the raw input reads (<b>{}</b>).'),
+    QCCheck(
+        key='nanoplot_len',
+        full_name='NanoPlot: Median read length (ONT)',
+        threshold_warn=500,
+        threshold_fail=250,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:,}'),
+    QCCheck(
+        key='nanoplot_qual',
+        full_name='NanoPlot: Median read quality (ONT)',
+        threshold_warn=10,
+        threshold_fail=8,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:.2f}'),
+    QCCheck(
+        key='seqkit_gc',
+        full_name='seqkit: GC-content deviation (ONT)',
+        threshold_warn=2.0,
+        threshold_fail=4.0,
+        supported_input_types=['hybrid', 'ont'],
+        fmt_string_value='{:.2f}%',
+        value_should_exceed=False,
+        explanation='checks if the detected GC content is close enough to the expected GC content for this organism'
+                    ' (<b>{:.2f}%</b>).')
 ]}
+
+
+def get_qc_checks(input_type: str, skipped_checks: List[str] = None) -> List[Path]:
+    """
+    Returns the output paths for the QC checks of the corresponding input type.
+    :param input_type: Input type
+    :param skipped_checks: (Optional) list of skipped QC checks
+    :return: List of paths with the enabled QC checks
+    """
+    paths = []
+    for key, qc_check in QC_CHECKS_BY_KEY.items():
+        if input_type not in qc_check.supported_input_types:
+            continue
+        if qc_check.is_default is False:
+            continue
+        if key in skipped_checks:
+            continue
+        if '{ori}' in key:
+            paths.extend([Path(_dir_qc, f'{key.format(ori=ori)}.json') for ori in ('fwd', 'rev')])
+        else:
+            paths.append(Path(_dir_qc, f'{key}.json'))
+    return paths
