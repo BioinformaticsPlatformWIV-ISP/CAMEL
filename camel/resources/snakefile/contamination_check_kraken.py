23a49ead1a92cbf452c51add2f079f060c059d6f
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-12-06 15:31:01 +0100

    Updated the contamination check scripts to handle more input types

--- camel/resources/snakefile/contamination_check_kraken.py
+++ camel/resources/snakefile/contamination_check_kraken.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List, Any
 
 SNAKEFILE_CONTAMINATION_CHECK_KRAKEN = f'{Path(__file__).parent / Path(__file__).stem}.smk'
-_dir_out = Path('contamination_check', '{read_type}')
+_dir_out = Path('contamination_check', '{read_key}')
 OUTPUT_CONTAMINATION_CHECK_REPORT = _dir_out / 'report' / 'html.io'
 OUTPUT_CONTAMINATION_CHECK_REPORT_EMPTY = _dir_out / 'report' / 'html-empty.io'
 OUTPUT_CONTAMINATION_CHECK_INFORMS = _dir_out / 'kraken2' / 'informs-contamination.io'
@@ -10,46 +10,68 @@ OUTPUT_CONTAMINATION_CHECK_KRAKEN_INFORMS = _dir_out / 'kraken2' / 'informs.io'
 OUTPUT_CONTAMINATION_SUMMARY = _dir_out / 'summary' / 'summary_out.tsv'
 
 
-def get_contamination_check_report(config, read_type: str = None) -> Path:
+def get_reports(config: Dict[str, Any]) -> List[Path]:
     """
-    Returns the Kraken report.
+    Returns the paths to the contamination check reports.
+    :param config: Snakemake configuration
+    :return: Report path(s)
+    """
+    input_type = config['input_type']
+    paths = []
+
+    # PE reads
+    if input_type in ('illumina', 'hybrid'):
+        if 'kraken2' in config['analyses']:
+            paths.append(str(OUTPUT_CONTAMINATION_CHECK_REPORT).format(read_key='fastq_pe'))
+        else:
+            paths.append(str(OUTPUT_CONTAMINATION_CHECK_REPORT_EMPTY).format(read_key='fastq_pe'))
+
+    # SE reads
+    if input_type in ('ont', 'hybrid'):
+        if 'kraken2' in config['analyses']:
+            paths.append(str(OUTPUT_CONTAMINATION_CHECK_REPORT).format(read_key='fastq_se'))
+        else:
+            paths.append(str(OUTPUT_CONTAMINATION_CHECK_REPORT_EMPTY).format(read_key='fastq_se'))
+    return [Path(config['working_dir']) / p for p in paths]
+
+
+def get_summaries(config: Dict[str, Any]) -> List[Path]:
+    """
+    Returns the paths to the contamination check summary file(s).
+    :param config: Snakemake configuration
+    :return: Report path(s)
+    """
+    input_type = config['input_type']
+    paths = []
+
+    # PE reads
+    if (input_type in ('illumina', 'hybrid')) and ('kraken2' in config['analyses']):
+        paths.append(str(OUTPUT_CONTAMINATION_SUMMARY).format(read_key='fastq_pe'))
+
+    # SE reads
+    if (input_type in ('ont', 'hybrid')) and ('kraken2' in config['analyses']):
+        paths.append(str(OUTPUT_CONTAMINATION_SUMMARY).format(read_key='fastq_se'))
+    return [Path(config['working_dir']) / p for p in paths]
+
+
+def get_command_informs(config: Dict[str, Any]) -> List[Path]:
+    """
+    Returns the paths to the Kraken informs.
     :param config: config
-    :param read_type: read_type
-    :return: Path to the Kraken informs
+    :return: Path(s) to the Kraken informs
     """
-    if read_type is None:
-        read_type = config['read_type']
-    if read_type not in ['illumina', 'nanopore']:
-        raise ValueError(f'Invalid read type: {read_type}')
-    if 'kraken' not in config['analyses']:
-        return Path(config['working_dir']) / str(OUTPUT_CONTAMINATION_CHECK_REPORT_EMPTY).format(read_type=read_type)
-    else:
-        return Path(config['working_dir']) / str(OUTPUT_CONTAMINATION_CHECK_REPORT).format(read_type=read_type)
+    input_type = config['input_type']
+    paths = []
 
+    # Kraken 2 is disabled -> return empty list
+    if 'kraken2' not in config['analyses']:
+        return []
 
-def get_contamination_check_summary(config: Dict, read_type: str = None) -> Path:
-    """
-    Returns the Kraken summary.
-    :param config: config
-    :param read_type: read_type
-    :return: Path to the Kraken informs
-    """
-    if read_type is None:
-        read_type = config['read_type']
-    if read_type not in ['illumina', 'nanopore']:
-        raise ValueError(f'Invalid read type: {read_type}')
-    return Path(config['working_dir']) / str(OUTPUT_CONTAMINATION_SUMMARY).format(read_type=read_type)
+    # PE reads
+    if input_type in ('illumina', 'hybrid'):
+        paths.append(str(OUTPUT_CONTAMINATION_CHECK_KRAKEN_INFORMS).format(read_key='fastq_pe'))
 
-
-def get_contamination_check_kraken_informs(config: Dict, read_type: str = None) -> Path:
-    """
-    Returns the Kraken informs.
-    :param config: config
-    :param read_type: read_type
-    :return: Path to the Kraken informs
-    """
-    if read_type is None:
-        read_type = config['read_type']
-    if read_type not in ['illumina', 'nanopore']:
-        raise ValueError(f'Invalid read type: {read_type}')
-    return Path(config['working_dir']) / str(OUTPUT_CONTAMINATION_CHECK_KRAKEN_INFORMS).format(read_type=read_type)
+    # SE reads
+    if input_type in ('ont', 'hybrid'):
+        paths.append(str(OUTPUT_CONTAMINATION_CHECK_KRAKEN_INFORMS).format(read_key='fastq_se'))
+    return [Path(config['working_dir']) / p for p in paths]
