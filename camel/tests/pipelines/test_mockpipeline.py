47284854c6421910dd638f3f4d9fa656253b1ad7
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-12-07 14:49:18 +0100

    Updated location of test files

--- camel/tests/pipelines/test_mockpipeline.py
+++ camel/tests/pipelines/test_mockpipeline.py
@@ -1,3 +1,4 @@
+import shutil
 import unittest
 from pathlib import Path
 
@@ -9,7 +10,9 @@ class TestMocksPipeline(CamelTestSuite):
     """
     Tests for the mock pipeline.
     """
-    test_file_dir = CamelTestSuite.get_test_file_dir('hybrid_assembly')
+    test_file_dir = CamelTestSuite.get_test_file_dir('pipelines', 'mock_pipeline')
+    input_ilmn_pe = [test_file_dir / 'ecoli_10k_ilmn_1.fastq.gz', test_file_dir / 'ecoli_10k_ilmn_2.fastq.gz']
+    input_ont_se = test_file_dir / 'ecoli_10k_ont.fastq.gz'
 
     def test_mock_pipeline_illumina(self) -> None:
         """
@@ -18,18 +21,16 @@ class TestMocksPipeline(CamelTestSuite):
         """
         path_report_out = Path(self.running_dir) / 'out' / 'report.html'
         path_summary_out = Path(self.running_dir) / 'out' / 'summary.tsv'
-
         pipeline = MainMockPipeline([
-            '--fastq-pe',
-            str(TestMocksPipeline.test_file_dir / 'ncbi_region_1.fastq'),
-            str(TestMocksPipeline.test_file_dir / 'ncbi_region_2.fastq'),
-            '--read-type', 'illumina',
+            '--input-type', 'illumina',
+            '--fastq-pe', *[str(x) for x in TestMocksPipeline.input_ilmn_pe],
             '--output-html', str(path_report_out),
             '--output-dir', str(path_report_out.parent),
             '--output-tsv', str(path_summary_out),
             '--working-dir', str(self.running_dir),
             '--detection-method', 'blast',
-            '--threads', '8',
+            '--ncbi-amr',
+            '--threads', '8'
         ])
         pipeline.run()
         self.assertGreater(path_report_out.stat().st_size, 0)
@@ -43,60 +44,41 @@ class TestMocksPipeline(CamelTestSuite):
         path_summary_out = Path(self.running_dir) / 'out' / 'summary.tsv'
 
         pipeline = MainMockPipeline([
-            '--fastq-se',
-            str(TestMocksPipeline.test_file_dir / 'ncbi_region_ont.fastq.gz'),
-            '--read-type', 'nanopore',
+            '--fastq-se', str(TestMocksPipeline.input_ont_se),
+            '--input-type', 'ont',
             '--output-html', str(path_report_out),
             '--output-dir', str(path_report_out.parent),
             '--output-tsv', str(path_summary_out),
             '--working-dir', str(self.running_dir),
             '--detection-method', 'blast',
+            '--ncbi-amr',
             '--threads', '8',
         ])
         pipeline.run()
         self.assertGreater(path_report_out.stat().st_size, 0)
 
-    # def test_mock_pipeline_hybrid(self) -> None:
-    #     """
-    #     Tests the mock pipeline with hybrid input data (Illumina + ONT).
-    #     :return: None
-    #     """
-    #     path_report_out = Path(self.running_dir) / 'out' / 'report.html'
-    #     path_summary_out = Path(self.running_dir) / 'out' / 'summary.tsv'
-    #
-    #     pipeline = MainMockPipeline([
-    #         '--fastq-pe', str(TestMocksPipeline.input_fastq_se_subtilis),
-    #         '--read-type', 'nanopore',
-    #         '--output-html', str(path_report_out),
-    #         '--output-dir', str(path_report_out.parent),
-    #         '--output-tsv', str(path_summary_out),
-    #         '--working-dir', str(self.running_dir),
-    #         '--detection-method', 'blast',
-    #         '--threads', '8',
-    #     ])
-    #     pipeline.run()
-    #     self.assertGreater(path_report_out.stat().st_size, 0)
-    #
-    # def test_mock_pipeline_fasta(self) -> None:
-    #     """
-    #     Tests the mock pipeline with FASTA input data.
-    #     :return: None
-    #     """
-    #     path_report_out = Path(self.running_dir) / 'out' / 'report.html'
-    #     path_summary_out = Path(self.running_dir) / 'out' / 'summary.tsv'
-    #
-    #     pipeline = MainMockPipeline([
-    #         '--fastq-pe', str(TestMocksPipeline.input_fastq_se_subtilis),
-    #         '--read-type', 'nanopore',
-    #         '--output-html', str(path_report_out),
-    #         '--output-dir', str(path_report_out.parent),
-    #         '--output-tsv', str(path_summary_out),
-    #         '--working-dir', str(self.running_dir),
-    #         '--detection-method', 'blast',
-    #         '--threads', '8',
-    #     ])
-    #     pipeline.run()
-    #     self.assertGreater(path_report_out.stat().st_size, 0)
+    def test_mock_pipeline_hybrid(self) -> None:
+        """
+        Tests the mock pipeline with hybrid input data (Illumina + ONT).
+        :return: None
+        """
+        path_report_out = Path(self.running_dir) / 'out' / 'report.html'
+        path_summary_out = Path(self.running_dir) / 'out' / 'summary.tsv'
+
+        pipeline = MainMockPipeline([
+            '--fastq-se', str(TestMocksPipeline.input_ont_se),
+            '--fastq-pe', *[str(x) for x in TestMocksPipeline.input_ilmn_pe],
+            '--input-type', 'hybrid',
+            '--output-html', str(path_report_out),
+            '--output-dir', str(path_report_out.parent),
+            '--output-tsv', str(path_summary_out),
+            '--working-dir', str(self.running_dir),
+            '--detection-method', 'blast',
+            '--ncbi-amr',
+            '--threads', '8',
+        ])
+        pipeline.run()
+        self.assertGreater(path_report_out.stat().st_size, 0)
 
 
 if __name__ == '__main__':
