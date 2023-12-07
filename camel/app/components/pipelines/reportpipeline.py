cdf54c22c8e9cd2d9b4f67ea41695f415ff968a8
Bert Bogaerts <bert.bogaerts@sciensano.be>
2023-12-07 17:33:09 +0100

    Added utility functions to the ReportPipeline class, added support for hybrid to the FastqInput class

--- camel/app/components/pipelines/reportpipeline.py
+++ camel/app/components/pipelines/reportpipeline.py
@@ -2,15 +2,18 @@ import abc
 import argparse
 import shutil
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, List, Tuple, Union
 
 from camel.app.components import mainscriptutils
 from camel.app.components.files.fastqutils import FastqUtils
 from camel.app.components.files.fileutils import FileUtils
 from camel.app.components.phylogeny.snpphylogenyutils import InvalidInputError
 from camel.app.components.pipelines.basepipeline import BasePipeline
+from camel.app.components.workflows.utils.fastqinput import FastqInput
+from camel.app.io.tooliovalue import ToolIOValue
 from camel.app.loggers import logger
 from camel.app.snakemake.snakemakeutils import SnakemakeUtils
+from camel.app.snakemake.snakepipelineutils import SnakePipelineUtils
 from camel.resources.snakefile import assembly_spades
 
 
@@ -103,3 +106,122 @@ class ReportPipeline(BasePipeline, metaclass=abc.ABCMeta):
         path_fasta = SnakemakeUtils.load_object(path_io)[0].path
         shutil.copyfile(path_fasta, self._args.output_fasta)
         logger.info(f'Output FASTA file copied to: {self._args.output_fasta}')
+
+    @staticmethod
+    def format_input_string(dict_in: Dict[str, Any]) -> str:
+        """
+        Formats the input string based on the dictionary with pipeline input files.
+        :param dict_in: Pipeline input dictionary
+        :return: Formatted input string
+        """
+        return ', '.join([entry['name'] for key, entries in dict_in.items() for entry in entries])
+
+    @staticmethod
+    def construct_fq_dict(snake_in: Any, input_type: str, path_out: Path) -> None:
+        """
+        Constructs a dictionary with the FASTQ input based on the snakemake input.
+        :param snake_in: Snakemake input
+        :param input_type: Input type
+        :param path_out: Path to store dictionary
+        :return: None
+        """
+        # PE reads (illumina)
+        if input_type == 'illumina':
+            shutil.copyfile(snake_in.FASTQ_PE, path_out)
+
+        # SE reads (iontorrent, ont)
+        elif input_type in ('iontorrent', 'ont'):
+            shutil.copyfile(snake_in.FASTQ_SE, path_out)
+
+        # Hybrid reads
+        elif input_type == 'hybrid':
+            fq_pe = FastqInput.from_fq_dict(Path(snake_in.FASTQ_PE), 'illumina')
+            fq_se = FastqInput.from_fq_dict(Path(snake_in.FASTQ_SE), 'ont')
+            SnakemakeUtils.dump_object(FastqInput(
+                'hybrid', pe=fq_pe.pe, se_fwd=fq_pe.se_fwd, se_rev=fq_pe.se_rev, se=fq_se.se, is_pe=True,
+                is_trimmed=True).to_fq_dict(), path_out)
+        else:
+            raise ValueError(f'Invalid input type: {input_type}')
+        logger.info(f'FASTQ dict object created: {path_out}')
+
+    @staticmethod
+    def export_command_section(snake_in: Any, path_out: Path, dir_: Path) -> None:
+        """
+        Generates the command report section and exports it to the target path.
+        :param snake_in: Snakemake input
+        :param path_out: Output path
+        :param dir_: Working directory (used to mask absolute paths)
+        :return :None
+        """
+        informs = []
+        for content in [SnakemakeUtils.load_object(Path(io)) for io in snake_in]:
+            if type(content) is dict:
+                informs.append(content)
+            elif type(content) is list:
+                informs.extend(content)
+        section = SnakePipelineUtils.create_commands_section(informs, dir_)
+        SnakemakeUtils.dump_object([ToolIOValue(section)], path_out)
+        logger.info(f'Command exported to: {path_out}')
+
+    @staticmethod
+    def add_content_trim_basic_qc(
+            structure: List[Tuple], input_type: str, reports_ds: List[Union[Path, str]],
+            reports_trim: List[Union[Path, str]]) -> None:
+        """
+        Adds the report content for the downsampling, basic QC, and read trimming.
+        :param structure: Report structure
+        :param input_type: Input type
+        :param reports_ds: Downsampling output reports
+        :param reports_trim: Trimming output reports
+        :return: None
+        """
+        # Create dictionaries with the technology as key and the reports as values
+        report_trim_by_tech = {
+            next(p.name.replace('trimming_', '') for p in p_html.parents if p.name.startswith('trimming_')):
+                p_html for p_html in [Path(x) for x in reports_trim]}
+        report_ds_by_read_key = {
+            p_html.parent.name: p_html for p_html in [Path(x) for x in reports_ds]}
+
+        # Add the report content
+        if input_type == 'illumina':
+            structure.append(('Read trimming and basic QC', 'trim', [
+                report_ds_by_read_key['fastq_pe'], report_trim_by_tech['illumina']]))
+        elif input_type == 'ont':
+            structure.append(('Read trimming and basic QC', 'trim', [
+                report_ds_by_read_key['fastq_se'], report_trim_by_tech['ont']]))
+        elif input_type == 'hybrid':
+            structure.append(('Read trimming and basic QC - Illumina', 'trim_ilmn', [
+                report_ds_by_read_key['fastq_pe'], report_trim_by_tech['illumina']]))
+            structure.append(('Read trimming and basic QC - ONT', 'trim_ont', [
+                report_ds_by_read_key['fastq_se'], report_trim_by_tech['ont']]))
+
+    @staticmethod
+    def add_content_contamination_check(
+            structure: List[Tuple], input_type: str, reports_contamination: List[Union[Path, str]],
+            report_confindr: Union[Path, str]) -> None:
+        """
+        Adds the report content for the contamination check.
+        :param structure: Report structure
+        :param input_type: Input type
+        :param reports_contamination: Contamination check output report(s)
+        :param report_confindr: ConFindr report
+        :return: None
+        """
+        # Create dictionaries with the technology as key and the reports as values
+        report_k2_by_read_key = {
+            p_html.parents[1].name: p_html for p_html in [Path(x) for x in reports_contamination]}
+
+        # Add the report content
+        if input_type == 'illumina':
+            structure.append(
+                ('Contamination check', 'contamination', [
+                    report_k2_by_read_key['fastq_pe'], Path(report_confindr)]))
+        elif input_type == 'ont':
+            structure.append(
+                ('Contamination check', 'contamination', [report_k2_by_read_key['fastq_se']]))
+        elif input_type == 'hybrid':
+            structure.append(
+                ('Contamination check', 'contamination',
+                 [report_k2_by_read_key['fastq_pe'], report_k2_by_read_key['fastq_se'], Path(report_confindr)]))
+        else:
+            raise ValueError(f'Invalid input type: {input_type}')
