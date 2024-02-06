f720da9f2ddcd2845b0019aaa0dc71709f8f2ae2
Bert Bogaerts <bert.bogaerts@sciensano.be>
2024-02-06 14:22:30 +0100

    Updated config template

--- camel/scripts/hybridassemblypipeline/mainhybridassemblypipeline.py
+++ camel/scripts/hybridassemblypipeline/mainhybridassemblypipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import argparse
 from pathlib import Path
-from typing import Optional, Sequence, List, Dict, Any, Tuple
+from typing import Dict, List, Optional, Sequence
 
 import pandas as pd
 import pkg_resources
@@ -9,7 +9,6 @@ import yaml
 
 from camel.app.camel import Camel
 from camel.app.components import mainscriptutils
-from camel.app.components.filesystemhelper import FileSystemHelper
 from camel.app.components.galaxy.galaxyutils import GalaxyUtils
 from camel.app.components.pipelines.basepipeline import BasePipeline
 from camel.app.loggers import logger
@@ -59,16 +58,9 @@ class MainHybridAssemblyPipeline(BasePipeline):
         :return: Arguments
         """
         argument_parser = argparse.ArgumentParser()
-
-        # Input
-        argument_parser.add_argument('--sample-name', type=str)
-        argument_parser.add_argument('--fastq-pe', type=Path, help='Input Fastq PE file', nargs=2, required=True)
-        argument_parser.add_argument('--fastq-pe-names', help='Input Fastq PE file', nargs=2)
-        argument_parser.add_argument('--fastq-se', type=Path, help='Input Fastq SE files')
-        argument_parser.add_argument('--fastq-se-name', help='Input Fastq SE file names')
+        BasePipeline.add_common_arguments(argument_parser)
 
         # Output
-        argument_parser.add_argument('--working-dir', type=Path, default=Path.cwd())
         argument_parser.add_argument('--output-html', type=Path, required=True)
         argument_parser.add_argument('--output-dir', type=Path)
 
@@ -95,12 +87,9 @@ class MainHybridAssemblyPipeline(BasePipeline):
         argument_parser.add_argument('--sniffles-mapq', type=int)
         argument_parser.add_argument('--sniffles-min-support', type=int)
 
-        # Logging & resources
-        argument_parser.add_argument(
-            '--galaxy-job-id', type=str, help='Job id of the run in galaxy (used for logging')
-        argument_parser.add_argument(
-            '--log', action='store_true', help="If this flag is set, config file and error logs are kept")
-        argument_parser.add_argument('--threads', type=int, default=8)
+        # Flag to enable Unicycler
+        argument_parser.add_argument('--unicycler', action='store_true', default=False)
+
         return argument_parser.parse_args(args)
 
     def run(self) -> None:
@@ -112,114 +101,50 @@ class MainHybridAssemblyPipeline(BasePipeline):
         path_config = self.__create_snakemake_config_data(input_files)
         self._run_snakemake_main(path_config)
 
-    def _get_fastq_input_links(self) -> List[List[Tuple[Path, str]]]:
-        """
-        Returns the links to the input FASTQ files.
-        :return: Links
-        """
-        links = []
-        input_fastq = self._args.fastq_pe
-        input_fastq.extend([self._args.fastq_se])
-        for read_nb, path in enumerate(input_fastq, start=1):
-            gzipped = FileSystemHelper.is_gzipped(path)
-            if read_nb > 2:
-                read_nb = 'ont'
-            links.append([path, f"{self._sample_name}_{read_nb}.fastq{'.gz' if gzipped else ''}"])
-        return links
-
-    def _symlink_input(self) -> List[Dict[str, Any]]:
-        """
-        Symlinks the input files.
-        :return: List of FASTQ input dictionaries
-        """
-        # Determine link names
-        links = self._get_fastq_input_links()
-
-        # Create directory
-        dir_links = self._args.working_dir / 'input'
-        if not dir_links.exists():
-            dir_links.mkdir(parents=True)
-
-        # Link files
-        paths_new = []
-        for path_orig, link_name in links:
-            path_new = dir_links / link_name
-            logger.debug(f"Symlinking input file: {path_orig} -> {link_name}")
-            if path_new.is_symlink():
-                path_new.unlink()
-            path_new.symlink_to(path_orig)
-            paths_new.append(path_new)
-
-        # Return output dictionary
-        return [{'name': p.name, 'path': p} for p in paths_new]
-
-    def __create_snakemake_config_data(self, input_fastq) -> str:
+    def __create_snakemake_config_data(self, input_files: Dict[str, List[Dict[str, str]]]) -> str:
         """
         Creates a Snakemake configuration file.
+        :input_files: Dictionary with the input files (keys can be FASTQ_PE, FASTQ_SE).
         :return: Config file data
         """
-        config_data = {
-            # Pipeline
-            'pipeline': {
-                'name': self.name,
-                'version': self.version,
-                'title': 'Hybrid assembly pipeline'
-            },
-            # Input
-            'sample_name': self._sample_name, 'working_dir': self._args.working_dir,
-            'input': {
-                'illumina': [str(input_fastq[0]['path'].absolute()), str(input_fastq[1]['path'].absolute())],
-                'ont': str(input_fastq[2]['path'].absolute())
-            },
-            # Output
-            'output_html': str(self._args.output_html.absolute()),
+        config_data = self.get_template_data(input_files)
+
+        # Add report specific entries
+        mainscriptutils.dict_merge(config_data, {
             'output_dir': str(self._args.output_dir.absolute()),
-            # Parameters
-            'filtlong': {
-                'keep_percent': self._args.filtlong_keep_percent if self._args.filtlong_keep_percent is not None else 95
-            },
-            'assembly': {
-                'flye': {
-                    'genome_size': self._args.expected_genome_size if self._args.expected_genome_size else False,
-                    'meta': True if self._args.flye_meta else False,
-                    'nano_corr': True if self._args.ont_qual == 'nano-corr' else False,
-                    'nano_hq': True if self._args.ont_qual == 'nano-hq' else False,
-                    'nano_raw': True if self._args.ont_qual == 'nano-raw' else False},
-                'min_contig_length': 1000,
-                'no_alt_contigs': True
-            },
-            'polishing': {
-                'medaka': {
-                    'consensus': {'model': self._args.ont_basecalling_model},
-                    'stitch': {}
-                },
-                'polca': {},
-                'polypolish': {}
-            },
-            'read_mapping': {
-                'bwa': {},
-                'minimap2': {}
-            },
-            'freebayes': {
-                'ploidy': self._args.ploidy,
-                'min_alternate_fraction': self._args.freebayes_min_alternate_fraction,
-                'min_alternate_count': self._args.freebayes_min_alternate_count
-            },
-            'clair3': {
-                'haploid_precise': True if self._args.ploidy == 1 else False,
-                'long_indel': True if self._args.clair3_long_indel is not None else False,
-                'model_path': str(self._get_clair3_model(self._args.ont_basecalling_model))
-            },
-            'sniffles': {
-                'mapq': self._args.sniffles_mapq if self._args.sniffles_mapq is not None else 40,
-                'min_support':
-                    self._args.sniffles_min_support if self._args.sniffles_min_support is not None else 'auto',
-                'min_svlen': self._args.sniffles_min_svlen if self._args.sniffles_min_svlen is not None else 35
-            }
-        }
+            'output_html': str(self._args.output_html.absolute())
+        })
+
+        # Assembly steps
+        config_data['assembly_steps'] = ['Flye', 'Medaka', 'Polypolish', 'POLCA']
+        config_data['base_assemblies'] = ['flye']
+        if self._args.unicycler:
+            config_data['assembly_steps'].extend(['Unicycler', 'Medaka-Unicycler', 'Polypolish-Unicycler', 'POLCA-Unicycler'])
+            config_data['base_assemblies'].extend(['unicycler'])
+
         with open(CONFIG_DATA) as handle_in:
             mainscriptutils.dict_merge(
-                config_data, yaml.safe_load(handle_in.read()))
+                config_data, yaml.safe_load(handle_in.read().format(
+                    keep_percent=self._args.filtlong_keep_percent if self._args.filtlong_keep_percent is not None
+                    else 95,
+                    genome_size=self._args.expected_genome_size if self._args.expected_genome_size else False,
+                    is_meta=True if self._args.flye_meta else False,
+                    nano_corr=True if self._args.ont_qual == 'nano-corr' else False,
+                    nano_hq=True if self._args.ont_qual == 'nano-hq' else False,
+                    nano_raw=True if self._args.ont_qual == 'nano-raw' else False,
+                    medaka_model=self._args.ont_basecalling_model,
+                    freebayes_min_alternate_count=self._args.freebayes_min_alternate_count,
+                    freebayes_min_alternate_fraction=self._args.freebayes_min_alternate_fraction,
+                    freebayes_ploidy=self._args.ploidy,
+                    clair3_haploid_precise=True if self._args.ploidy == 1 else False,
+                    clair3_long_indel=True if self._args.clair3_long_indel is not None else False,
+                    clair3_model_path=str(self._get_clair3_model(self._args.ont_basecalling_model)),
+                    sniffles_mapq=self._args.sniffles_mapq if self._args.sniffles_mapq is not None else 40,
+                    sniffles_min_support=self._args.sniffles_min_support if self._args.sniffles_min_support is not None
+                    else 'auto',
+                    sniffles_min_svlen=self._args.sniffles_min_svlen if self._args.sniffles_min_svlen is not None
+                    else 35
+                )))
         return SnakePipelineUtils.generate_config_file(config_data, self._args.working_dir)
 
     def _get_clair3_model(self, medaka_model: str) -> Path:
