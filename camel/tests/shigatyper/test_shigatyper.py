24d9fdaa5fbec6aafa654a6b0a2455a740f58ae5
nagoeders <VSeB!2022>
2024-02-13 16:35:00 +0100

    Minor changes based on code review

--- camel/tests/shigatyper/test_shigatyper.py
+++ camel/tests/shigatyper/test_shigatyper.py
@@ -11,25 +11,23 @@ from camel.app.tools.pipelines.shigella.shigatyperreporter import ShigaTyperRepo
 
 class TestShigaTyper(CamelTestSuite):
     """
-    Tests the ShigaTyper tool.
+    Tests the ShigaTyper tool and the related reporter tool.
     """
     test_file_dir = CamelTestSuite.get_test_file_dir('pipelines')
     fastq_fwd = test_file_dir / 'Shigella-S17BD07654_1.fastq.gz'
     fastq_rev = test_file_dir / 'Shigella-S17BD07654_2.fastq.gz'
-
-    sample_id = fastq_fwd.name.replace('_1.fastq.gz', '')
 
     def test_shigatyper(self) -> None:
         """
         Tests the ShigaTyper tool.
         :return: None
         """
-        serotype_tool = ShigaTyper(self.camel)
-        serotype_tool.add_input_files({'FASTQ_FWD': [ToolIOFile(Path(TestShigaTyper.fastq_fwd))],
-                                       'FASTQ_REV': [ToolIOFile(Path(TestShigaTyper.fastq_rev))]})
-        serotype_tool.run(self.running_dir)
-        logging.info(f'Successfully processed: {TestShigaTyper.sample_id}')
-        self.verify_output_files(serotype_tool, 'TSV')
+        shigatyper = ShigaTyper(self.camel)
+        shigatyper.add_input_files({'FASTQ_FWD': [ToolIOFile(Path(TestShigaTyper.fastq_fwd))],
+                                    'FASTQ_REV': [ToolIOFile(Path(TestShigaTyper.fastq_rev))]})
+        shigatyper.run(self.running_dir)
+        self.verify_output_files(shigatyper, 'TSV')
+        self.verify_output_files(shigatyper, 'TSV_HITS')
 
     def test_shigatyper_reporter(self) -> None:
         """
@@ -37,16 +35,16 @@ class TestShigaTyper(CamelTestSuite):
         :return: None
         """
         # Run the tool
-        serotype_tool = ShigaTyper(self.camel)
-        serotype_tool.add_input_files({'FASTQ_FWD': [ToolIOFile(Path(TestShigaTyper.fastq_fwd))],
-                                       'FASTQ_REV': [ToolIOFile(Path(TestShigaTyper.fastq_rev))]})
-        serotype_tool.run(self.running_dir)
+        shigatyper = ShigaTyper(self.camel)
+        shigatyper.add_input_files({'FASTQ_FWD': [ToolIOFile(Path(TestShigaTyper.fastq_fwd))],
+                                    'FASTQ_REV': [ToolIOFile(Path(TestShigaTyper.fastq_rev))]})
+        shigatyper.run(self.running_dir)
 
         # Run the reporter
         reporter = ShigaTyperReporter(self.camel)
-        reporter.add_input_files({'TSV': serotype_tool.tool_outputs['TSV'],
-                                  'TSV_HITS': serotype_tool.tool_outputs['TSV_HITS']})
-        reporter.add_input_informs({'shigatyper': serotype_tool.informs})
+        reporter.add_input_files({'TSV': shigatyper.tool_outputs['TSV'],
+                                  'TSV_HITS': shigatyper.tool_outputs['TSV_HITS']})
+        reporter.add_input_informs({'shigatyper': shigatyper.informs})
         reporter.run(self.running_dir)
         self.assertGreater(len(reporter.tool_outputs['HTML'][0].value.to_html()), 0)
 
