4aff5c9c9d590b7ca5a0152a2e5db5b9bf6f43ed
Bert Bogaerts <bert.bogaerts@sciensano.be>
2024-03-06 09:08:03 +0100

    Restored bcftools mpileup piping functionality

--- camel/app/tools/bcftools/bcftoolsmpileup.py
+++ camel/app/tools/bcftools/bcftoolsmpileup.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from typing import Optional
 
 from camel.app.camel import Camel
 from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
 from camel.app.error.toolexecutionerror import ToolExecutionError
 from camel.app.io.tooliofile import ToolIOFile
-from camel.app.tools.tool import Tool
+from camel.app.tools.toolpipeable import ToolPipeable
 
 
-class BcftoolsMpileup(Tool):
+class BcftoolsMpileup(ToolPipeable):
     """
     Multi-way pileup producing genotype likelihoods.
     """
@@ -46,25 +47,33 @@ class BcftoolsMpileup(Tool):
         else:
             return 'VCF'
 
+    def __get_output_path(self) -> Path:
+        """
+        Returns the path to the output file.
+        :return: Output path
+        """
+        return self.folder / self._parameters['output_filename'].value
+
     def _execute_tool(self) -> None:
         """
         Executes this tool.
         :return: None
         """
-        path_out = self.folder / self._parameters['output_filename'].value
-        self._build_command(path_out)
+        self._build_command(self.__get_output_path())
         self._execute_command()
-        self._tool_outputs[self.__get_output_key()] = [ToolIOFile(path_out)]
+        self._tool_outputs[self.__get_output_key()] = [ToolIOFile(self.__get_output_path())]
 
-    def _build_command(self, path_out: Path) -> None:
+    def _build_command(self, path_out: Optional[Path], pipe_in: bool = False, pipe_out: bool = False) -> None:
         """
         Builds the command that is called.
+        :param pipe_in: True if the tool is the input of a pipe
+        :param pipe_out: True if the tool is the output of a pipe
         :param path_out: Output path
         :return: None
         """
         command_parts = [
             self._tool_command,
-            str(self._tool_inputs['BAM'][0].path),
+            str(self._tool_inputs['BAM'][0].path) if not pipe_in else '-',
             f"--fasta-ref {self._tool_inputs['FASTA'][0].path}"
         ]
         if 'BED_include' in self._tool_inputs:
@@ -72,7 +81,8 @@ class BcftoolsMpileup(Tool):
         elif 'BED_exclude' in self._tool_inputs:
             command_parts.append(f"--targets-file ^{self._tool_inputs['BED_exclude'][0].path}")
         command_parts.extend(self._build_options(excluded_parameters=['output_filename']))
-        command_parts.extend(['--output', str(path_out)])
+        if not pipe_out:
+            command_parts.extend(['--output', str(path_out)])
         self._command.command = ' '.join(command_parts)
 
     def _check_command_output(self) -> None:
@@ -81,3 +91,23 @@ class BcftoolsMpileup(Tool):
         """
         if self._command.returncode != 0:
             raise ToolExecutionError(f'Error executing {self._name}: {self._command.stderr}')
+
+    def _before_pipe(self, dir_: Path, pipe_in: bool, pipe_out: bool) -> None:
+        """
+        Prepares the command that will be piped.
+        :param dir_: Running directory
+        :param pipe_in: True if tool receives piped input
+        :param pipe_out: True if tool generates piped output
+        :return: None
+        """
+        self._build_command(self.__get_output_path(), pipe_in, pipe_out)
+
+    def _after_pipe(self, stderr: str, is_last_in_pipe: bool) -> None:
+        """
+        Performs the required steps after executing the tool as part of a pipe.
+        :param stderr: Stderr for this command in the pipe
+        :param is_last_in_pipe: Boolean to indicate if this is the last step in the pipe
+        :return: None
+        """
+        if is_last_in_pipe:
+            self._tool_outputs[self.__get_output_key()] = [ToolIOFile(self.__get_output_path())]
