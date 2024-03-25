c51d2ac42166513d5fc28eb799b3a517f298ccbb
MaGo3828 <maxime.godfroid@sciensano.be>
2024-03-25 12:02:07 +0100

    [mg] last fixes from Bert's comments to the bacillus pipeline

--- camel/app/tools/pipelines/bacillus/updategmmreport.py
+++ camel/app/tools/pipelines/bacillus/updategmmreport.py
@@ -8,7 +8,11 @@ from camel.app.tools.tool import Tool
 
 
 class UpdateGMMReport(Tool):
+    """
+    Updates the GMM gene detection report with warning about detected GMMs.
+    """
     INPUT_KEYS = ['TSV_STRAINS', 'TSV_GMM', 'VAL_HTML', 'TSV_GMM_DB']
+    color_code = {'STRAIN_MATCH': 'green', 'GMM_MATCH': 'yellow', 'BOTH_MATCH': 'red'}
 
     def __init__(self, camel: Camel) -> None:
         """
@@ -18,7 +22,7 @@ class UpdateGMMReport(Tool):
         """
         super().__init__('UpdateGMMReport', '0.1', camel)
 
-    def _check_inputs(self) -> None:
+    def _check_input(self) -> None:
         """
         Checks the input.
         :return: None
@@ -33,7 +37,6 @@ class UpdateGMMReport(Tool):
         Executes the tool.
         :return: None
         """
-        self._check_inputs()
         self._parse_tsv_files()
         output_report = self._update_report()
         self._tool_outputs['VAL_HTML'] = [ToolIOValue(output_report)]
@@ -41,10 +44,9 @@ class UpdateGMMReport(Tool):
     def _update_report(self) -> HtmlReportSection:
         """
         Updates the report with the GMM warning table.
-        :return: None
+        :return: Updated report section
         """
         matches = self._parse_tsv_files()
-        color_code = {'STRAIN_MATCH': 'green', 'GMM_MATCH': 'yellow', 'BOTH_MATCH': 'red'}
         current_report_section = self._tool_inputs['VAL_HTML'][0].value
         current_report_section.add_header('Interpretation', level=4)
         if not (matches['strain'] and matches['construct']):
@@ -56,16 +58,22 @@ class UpdateGMMReport(Tool):
         current_report_section.add_table(table_to_add, column_names, [('class', 'data')])
 
         if matches['strain']:
-            current_report_section.add_paragraph(f'Presence of strain {matches["strain"][0]} indicates a species '
-                                                 f'frequently subjected to genetic modifications.')
+            current_report_section.add_paragraph(f'The strain matches closely to <b>{matches["strain"][0]}</b> '
+                                                 f'which has been used in GMMs.')
+        else:
+            current_report_section.add_paragraph('The strain does not match any known GMM strains in the database.')
         if matches['construct']:
-            current_report_section.add_paragraph(f'Presence of construct {matches["construct"][0]} indicates '
-                                                 f'a frequent transgenic construct used in genetic'
-                                                 f'modifications of micro-organisms.')
+            current_report_section.add_paragraph(f'The <b>{matches["construct"][0]}</b> transgenic construct '
+                                                 f'was detected in the strain.')
+        else:
+            current_report_section.add_paragraph('No transgenic constructs from the database were detected in the strain.')
 
-        current_report_section.add_warning_message('Presence of such markers necessitates further '
-                                                   'testing to ensure presence of an actual '
-                                                   'genetically modified micro-organism.')
+        if matches['strain'] and matches['construct']:
+            current_report_section.add_paragraph('<b>IMPORTANT: A transgenic construct was detected in your sample.</b>')
+
+        current_report_section.add_warning_message('The pipeline uses a targeted approach, which means that constructs '
+                                                   'and/or strains that are not in the database will be missed.')
+
         return current_report_section
 
     def _parse_tsv_files(self) -> dict:
@@ -75,16 +83,11 @@ class UpdateGMMReport(Tool):
         """
         tsv_gmm_db = pd.read_csv(self._tool_inputs['TSV_GMM_DB'][0].path)
 
-        print("HERE")
-        print(tsv_gmm_db)
-
         strain_hits = []
         for f in self._tool_inputs['TSV_STRAINS']:
             with open(f.path) as handle:
                 for line in handle:
                     spl = line.strip().split()
-                    print("HERE!!!")
-                    print(spl)
                     if 'closest_strain' in spl[0]:
                         if spl[1] in tsv_gmm_db['strain'].tolist():
                             strain_hits.append(spl[1])
@@ -94,10 +97,6 @@ class UpdateGMMReport(Tool):
             all_lines = handle.readlines()
             gmm_hits_list = eval(all_lines[0].strip().split('\t')[1])
             for entry in gmm_hits_list:
-                print("HERE!!!!!")
-                print(entry)
-                print(entry[1] in tsv_gmm_db['construct'])
-                print(entry[1] in tsv_gmm_db['construct'])
                 if entry[1] in tsv_gmm_db['construct'].tolist():
                     gmm_hits.append(entry[1])
 
