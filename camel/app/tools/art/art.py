b2dacb6b138f5e849c3fce1b123e6148f9a8bd1a
kamilis <kato.milis@sciensano.be>
2024-03-29 15:12:22 +0100

    completion of ART implementation + test ART

--- camel/app/tools/art/art.py
+++ camel/app/tools/art/art.py
@@ -1,12 +1,12 @@
-import re
 from pathlib import Path
 
-from camel.app.tools.tool import Tool
 from camel.app.camel import Camel
-from camel.app.io.tooliofile import ToolIOFile
+from camel.app.command.command import Command
 from camel.app.components.filesystemhelper import FileSystemHelper
 from camel.app.error.toolexecutionerror import ToolExecutionError
-
+from camel.app.io.tooliofile import ToolIOFile
+from camel.app.loggers import logger
+from camel.app.tools.tool import Tool
 
 
 class ART(Tool):
@@ -18,9 +18,9 @@ class ART(Tool):
         """
         Initializes this tool.
         :param camel: CAMEL instance
+        :return: None
         """
         super().__init__('ART', '2.5.8', camel)
-        #TODO: support single-end read simulation mode as well?
 
     def _execute_tool(self) -> None:
         """
@@ -33,24 +33,23 @@ class ART(Tool):
         # Compress the two output files
         FileSystemHelper.gzip_file(self.__get_output_path('1'), Path(f"{self.__get_output_path('1')}.gz"))
         FileSystemHelper.gzip_file(self.__get_output_path('2'), Path(f"{self.__get_output_path('2')}.gz"))
-        #TODO: delete .fq files?
-        # Set camel output and informs
+        # Remove the uncompressed FASTQ files
+        self.__remove_file(self.__get_output_path('1'))
+        self.__remove_file(self.__get_output_path('2'))
+        # Set the output
         self.__set_output()
-        self.__set_informs()
-        if not self._informs.get('succeed', 'False'):
-            raise ToolExecutionError("Error running ART")
 
     def _check_input(self) -> None:
         """
         Checks the input.
         :return: None
         """
+        super()._check_input()
         if 'FASTA' in self._tool_inputs:
             if len(self._tool_inputs['FASTA']) != 1:
                 raise ValueError("FASTA input requires exactly 1 file.")
         else:
-            raise ValueError("No FASTA input found")
-        super(ART, self)._check_input()
+            raise ValueError("FASTA input is required")
 
     def __build_command(self) -> None:
         """
@@ -59,31 +58,27 @@ class ART(Tool):
         """
         self._command.command = ' '.join([
             self._tool_command,
-            '-ss', f"{self._parameters['sequencing_system'].value}",
-            '-i', ' '.join(str(io.path) for io in self._tool_inputs['FASTA']),
-            '-p', # Run a paired-end simulation
-            '-l', f"{self._parameters['read_length'].value}",
-            '-f', f"{self._parameters['fold_coverage'].value}",
-            '-m', f"{self._parameters['mean_size'].value}",
-            '-s', f"{self._parameters['standard_deviation'].value}",
-            '-o', f"{self._parameters['out'].value}",
-            '-na' # Do not generate an ALN alignment file
-        ])
+            f"-i {self._tool_inputs['FASTA'][0].path}",
+            "-p",  # paired-end
+            "-na",  # do not generate an ALN alignment file,
+            *self._build_options()]
+        )
 
     def _check_command_output(self) -> None:
         """
         Checks if the command was executed successfully.
         :return: None
         """
-        if re.match("Warning: your simulation will not output any ALN", self.stderr):
-            # The warning can be ignored because we don't need ALN files
-            pass
-        else:
-            super(ART, self)._check_command_output()
+        if 'error' in self.stderr.lower():
+            raise ToolExecutionError(f"Command execution failed (stderr: {self.stderr}).")
+        if self._command.returncode != 0:
+            raise ToolExecutionError(f"Command execution failed (Exit code: {self._command.returncode})")
 
     def __get_output_path(self, suffix: str) -> Path:
         """
         Returns the path for the output file with the given suffix.
+        :param suffix: suffix for the filename
+        :return: Path
         """
         basename = self._parameters['out'].value
         return self.folder / f"{FileSystemHelper.make_valid(basename)}{suffix}.fq"
@@ -96,20 +91,15 @@ class ART(Tool):
         self._tool_outputs['FASTQ'] = [
             ToolIOFile(Path(f"{self.__get_output_path('1')}.gz")), ToolIOFile(Path(f"{self.__get_output_path('2')}.gz"))]
 
-    def __set_informs(self) -> None:
+    @staticmethod
+    def __remove_file(input_file: Path) -> None:
         """
-        Adds to the informs whether execution succeeded.
+        Removes a file.
+        :param input_file: Input file
         :return: None
         """
-        #TODO: is there a better way to do this? --> not necessary
-        for line in self.stdout.splitlines():
-            if line.strip() == 'Output files':
-                self._informs['succeed'] = True
-
-if __name__ == '__main__':
-    camel = Camel.get_instance()
-    tool = ART(camel)
-    tool.add_input_files({'FASTA': [ToolIOFile(Path('/db/refgenomes/Yersinia_enterocolitica/NC_GCA_02575835.1.fasta'))]})
-    tool.run()
-    print("OUTPUT: ",tool._tool_outputs)
-    print("INFORMS: ", tool._informs)
+        logger.info(f"Removing: {input_file}")
+        command = Command(f"rm {input_file}")
+        command.run(Path.cwd())
+        if not command.returncode == 0:
+            raise RuntimeError(f"Cannot remove '{input_file}': {command.stderr}")
---
+++ camel/tests/art/test_art.py
@@ -0,0 +1,31 @@
+import unittest
+from pathlib import Path
+
+from camel.app.components.testing.cameltestsuite import CamelTestSuite
+from camel.app.io.tooliofile import ToolIOFile
+from camel.app.tools.art.art import ART
+
+
+class TestART(CamelTestSuite):
+    """
+    Tests the ART tool.
+    """
+    # Input files
+    test_file_dir = CamelTestSuite.get_test_file_dir('snippy')
+    input_fasta = test_file_dir / 'S32BD03301_contigs_unfilt.fasta'
+
+    def test_art(self) -> None:
+        """
+        Tests the ART tool.
+        :return: None
+        """
+        art = ART(self.camel)
+        art.add_input_files({
+            'FASTA': [ToolIOFile(Path(TestART.input_fasta))]
+        })
+        art.run(self.running_dir)
+        self.verify_output_files(art, 'FASTQ', 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
