6caa00689881a5f2b72d210923b09316e5fd9a88
nagoeders <nathalie.goeders@sciensano.be>
2024-05-02 16:11:03 +0200

    Added typing loci to the quality checks

--- camel/scripts/shigellapipeline/mainshigellapipeline.py
+++ camel/scripts/shigellapipeline/mainshigellapipeline.py
@@ -56,7 +56,8 @@ class MainShigellaPipeline(ReportPipeline):
         with open(CONFIG_DATA) as handle_in:
             config_data.update(yaml.load(handle_in.read().format(
                 coverage_max=self._args.cov_max,
-                export_fastq='true' if self._args.report_include_fastq else 'false'
+                export_fastq='true' if self._args.report_include_fastq else 'false',
+                qc_typing_scheme='cgmlst' if self._args.cgmlst else 'mlst_warwick'
             ), Loader=yaml.SafeLoader))
         return SnakePipelineUtils.generate_config_file(config_data, self._args.working_dir)
 
