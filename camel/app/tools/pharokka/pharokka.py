fe8c7257b6079574f1190a3c915b3f822157963c
nagoeders <nathalie.goeders@sciensano.be>
2024-10-25 14:53:36 +0200

    Updated code based on BB's feedback

--- camel/app/tools/pharokka/pharokka.py
+++ camel/app/tools/pharokka/pharokka.py
@@ -1,10 +1,9 @@
-import os
-import re
 import pandas as pd
 from pathlib import Path
 
 from camel.app.camel import Camel
 from camel.app.error.toolexecutionerror import ToolExecutionError
+from camel.app.error.invalidinputspecificationerror import InvalidInputSpecificationError
 from camel.app.io.tooliofile import ToolIOFile
 from camel.app.tools.tool import Tool
 
@@ -13,6 +12,14 @@ class Pharokka(Tool):
     """
     Pharokka is a fast phage genome annotation tool.
     """
+
+    OUTPUT_DICT = {
+        'GBK': 'pharokka.gbk',
+        'TSV_CARD': 'top_hits_card.tsv',
+        'TSV_VFDB': 'top_hits_vfdb.tsv',
+        'TSV_STATS': 'pharokka_cds_functions.tsv',
+        'TSV_INPHARED': 'pharokka_top_hits_mash_inphared.tsv'
+    }
 
     def __init__(self, camel: Camel.get_instance()) -> None:
         """
@@ -30,18 +37,16 @@ class Pharokka(Tool):
         self._execute_command()
 
         # Collect the output
-        for key, basename in zip(('GBK', 'CARD', 'VFDB', 'STATS', 'INPHARED'),
-                                 ('pharokka.gbk', 'top_hits_card.tsv', 'top_hits_vfdb.tsv',
-                                  'pharokka_cds_functions.tsv', 'pharokka_top_hits_mash_inphared.tsv')):
-            path_out = self.folder / 'pharokka' / basename
+        for key, basename in Pharokka.OUTPUT_DICT.items():
+            path_out = Path(self.folder, self._parameters['outdir'].value, basename)
             if not path_out.exists():
                 raise ToolExecutionError(f'{path_out} not generated ({key})')
             self._tool_outputs[key] = [ToolIOFile(path_out)]
 
         # Parse output files
-        self._parse_card_hits(self._tool_outputs['CARD'][0].path)
-        self._parse_vfdb_hits(self._tool_outputs['VFDB'][0].path)
-        self._parse_metrics(self._tool_outputs['STATS'][0].path)
+        self._parse_card_hits(self._tool_outputs['TSV_CARD'][0].path)
+        self._parse_vfdb_hits(self._tool_outputs['TSV_VFDB'][0].path)
+        self._parse_metrics(self._tool_outputs['TSV_STATS'][0].path)
 
     def _check_input(self) -> None:
         """
@@ -49,10 +54,10 @@ class Pharokka(Tool):
         :return: None
         """
         if 'FASTA' not in self._tool_inputs:
-            raise ValueError("No FASTA input found")
+            raise InvalidInputSpecificationError("No FASTA input found")
         if len(self._tool_inputs['FASTA']) != 1:
-            raise ValueError("Only one FASTA file can be annotated at a time.")
-        super(Pharokka, self)._check_input()
+            raise InvalidInputSpecificationError("Only one FASTA file can be annotated at a time.")
+        super()._check_input()
 
     def _check_command_output(self) -> None:
         """
@@ -60,7 +65,7 @@ class Pharokka(Tool):
         :return: None
         """
         if self._command.returncode != 0:
-            raise ToolExecutionError("Error executing Pharokka: {}".format(self.stderr))
+            raise ToolExecutionError(f"Error executing Pharokka: {self.stderr}")
 
     def __build_command(self) -> None:
         """
@@ -69,41 +74,41 @@ class Pharokka(Tool):
         """
         self._command.command = ' '.join([
             self._tool_command,
-            ' '.join(self._build_options()),
-            f'-i',
+            *self._build_options(),
+            '-i',
             str(self._tool_inputs['FASTA'][0].path)
         ])
 
-    def _parse_metrics(self, tsv_path: Path) -> None:
+    def _parse_metrics(self, path_tsv: Path) -> None:
         """
         Parses the file with the summary metrics and stores them in informs.
-        :param path_csv: Path to output file
+        :param path_tsv: Path to output file
         :return: None
         """
-        data = pd.read_table(tsv_path)
+        data = pd.read_table(path_tsv)
         cds_count = data.iloc[0]['Count']
         self._informs['stats'] = f'{cds_count} CDS were annotated by Pharokka.'
 
-    def _parse_card_hits(self, tsv_path: Path) -> None:
+    def _parse_card_hits(self, path_tsv: Path) -> None:
         """
         Parses the top CARD hits TSV file and stores the results in the informs.
-        :param path_csv: Path to output file
+        :param path_tsv: Path to output file
         :return: None
         """
-        data = pd.read_table(tsv_path)
+        data = pd.read_table(path_tsv)
         if data.empty:
-            self._informs['card_hits'] = 'No AMR gene was detected with the CARD database.'
+            self._informs['card_hits'] = 'No AMR gene was detected.'
         else:
-            self._informs['card_hits'] = f'{data.shape[0]} AMR genes were detected with the CARD database.'
+            self._informs['card_hits'] = f'{len(data)} AMR genes were detected.'
 
-    def _parse_vfdb_hits(self, tsv_path: Path) -> None:
+    def _parse_vfdb_hits(self, path_tsv: Path) -> None:
         """
         Parses the top VFDB hits TSV file and stores the results in the informs.
-        :param path_csv: Path to output file
+        :param path_tsv: Path to output file
         :return: None
         """
-        data = pd.read_table(tsv_path)
+        data = pd.read_table(path_tsv)
         if data.empty:
-            self._informs['vfdb_hits'] = 'No virulence gene was detected with the VFDB database.'
+            self._informs['vfdb_hits'] = 'No virulence gene was detected.'
         else:
-            self._informs['vfdb_hits'] = f'{data.shape[0]} virulence genes were detected with the VFDB database.'
+            self._informs['vfdb_hits'] = f'{len(data)} virulence genes were detected.'
