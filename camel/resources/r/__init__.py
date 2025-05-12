00c9980c286f04c42785c7d4feb6bacbebc4dfd0
Bert Bogaerts <bert.bogaerts@sciensano.be>
2025-05-12 11:26:35 +0200

    Removed unused class and resources

--- camel/resources/r/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-_current_dir = os.path.dirname(__file__)
-
-RSCRIPT_QC_COVERAGE = os.path.join(_current_dir, 'qc_visualize_coverage.r')
-RSCRIPT_QC_MAPPING_RATE = os.path.join(_current_dir, 'qc_visualize_mapping_rate.r')
-RSCRIPT_QC_ST = os.path.join(_current_dir, 'qc_visualize_st.r')
