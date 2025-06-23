b3cf7e72342fc8b2c9c4fb6c93e7b9d1c0330745
Bert Bogaerts <bert.bogaerts@sciensano.be>
2025-06-23 13:23:09 +0200

    bugfix for distance filter

--- camel/app/tools/variantfiltering/distancefilter.py
+++ camel/app/tools/variantfiltering/distancefilter.py
@@ -71,13 +71,14 @@ class DistanceFilter(BaseFilter):
         :return: None
         """
         file_parameter = '--targets-file ^' if 'soft_filter' not in self._parameters else '--mask-file '
-        self._command.command = ' '.join([
-            self._tool_command,
+        parts = [self._tool_command,
             str(self._tool_inputs['VCF_GZ'][0].path),
             '--output-type z',
-            f'--output {self.output_path}',
-            *(f"{file_parameter}{str(bed_file)}" if bed_file is not None else ())
-        ] + self._get_soft_filter_options())
+            f'--output {self.output_path}'
+        ]
+        if bed_file is not None:
+            parts.append(f"{file_parameter}{str(bed_file)}")
+        self._command.command = ' '.join(parts + self._get_soft_filter_options())
 
     @staticmethod
     def __create_bed_file(positions_to_filter: list[tuple[str, int]], bed_file: Path) -> None:
